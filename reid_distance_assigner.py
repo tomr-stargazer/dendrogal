4a26ec90de497e51728a88a134498760e61611db
Tom Rice <t.rice90@gmail.com>
2014-02-05 12:52:31 -0500

    Revised test string and now I rename the output columns too.

--- reid_distance_assigner.py
+++ reid_distance_assigner.py
@@ -12,7 +12,7 @@ from subprocess import Popen, PIPE, STDOUT
 
 import astropy.table
 
-test_string = "lol    010203.04 121314.5 123 0"
+test_string = "test    010203.04 121314.5 -10 1"
 
 f = open('source_file.dat', 'w')
 f.write(test_string)
@@ -29,5 +29,20 @@ print error
 
 lines_of_data = [x for x in output.split('\n') if '!' not in x]
 
-astropy.table.Table.read(lines_of_data, format='ascii')
+kd_output = astropy.table.Table.read(lines_of_data, format='ascii')
 
+"""
+! Source     Gal Long  Gal Lat    V_lsr     V_rev    Rev. D_k     +/-
+!              (deg)    (deg)    (km/s)    (km/s)     (kpc)      (kpc)
+"""
+
+kd_output.rename_column('col1', 'Source')
+kd_output.rename_column('col2', 'gal_long')
+kd_output.rename_column('col3', 'gal_lat')
+kd_output.rename_column('col4', 'V_lsr')
+kd_output.rename_column('col5', 'V_rev')
+kd_output.rename_column('col6', 'D_k')
+kd_output.rename_column('col7', 'error_D_k_plus')
+kd_output.rename_column('col8', 'error_D_k_minus')
+
+print kd_output
