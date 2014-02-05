1199ef1387bf87d6d2cce497ef968bcb91c84168
Tom Rice <t.rice90@gmail.com>
2014-02-05 12:34:20 -0500

    Got some more functionality working.

--- reid_distance_assigner.py
+++ reid_distance_assigner.py
@@ -8,26 +8,26 @@ Hopefully I can figure out how to do this in a non-boneheaded way.
 from __future__ import division
 
 import subprocess
-
 from subprocess import Popen, PIPE, STDOUT
 
-#p = Popen(['grep', 'f'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+import astropy.table
 
-#grep_stdout = p.communicate(input='one\ntwo\nthree\nfour\nfive\nsix\n')[0]
-
-#StdinCommand = '''
-#    MolPro code
-#'''
-
-# given some kind of input variables, generate the string or file 
-# that revised_kinematic_distance.f needs
-
-#   name rastring destring vstring fstring
 test_string = "lol    010203.04 121314.5 123 0"
 
-p = Popen(['/Users/tsrice/Documents/Code/mark_reid_kdist/revised_kinematic_distance'], 
-          shell = False, stdin = PIPE)
-output, error = p.communicate(input = test_string)
+f = open('source_file.dat', 'w')
+f.write(test_string)
+f.close()
+
+p = Popen(
+    ['/Users/tsrice/Documents/Code/mark_reid_kdist/revised_kinematic_distance'],
+    shell = False, stdin=PIPE, stdout=PIPE)
+
+output, error = p.communicate()
 
 print output
 print error
+
+lines_of_data = [x for x in output.split('\n') if '!' not in x]
+
+astropy.table.Table.read(lines_of_data, format='ascii')
+
