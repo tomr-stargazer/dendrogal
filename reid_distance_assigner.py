413b5eb3b20dabdc8829896f3d1952ccfc6c56cc
Tom Rice <t.rice90@gmail.com>
2014-02-04 19:35:36 -0500

    making a baby code file, nothing in here yet

---
+++ reid_distance_assigner.py
@@ -0,0 +1,14 @@
+"""
+Some python code to interface with Mark Reid's fortran tool.
+
+Hopefully I can figure out how to do this in a non-boneheaded way.
+
+"""
+
+from __future__ import division
+
+import subprocess
+
+# given some kind of input variables, generate the string or file 
+# that revised_kinematic_distance.f needs
+
