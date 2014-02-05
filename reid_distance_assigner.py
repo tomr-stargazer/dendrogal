ce0c8cc65f492e0825f9eb26fcea0b575a9deaa5
Tom Rice <t.rice90@gmail.com>
2014-02-05 00:25:09 -0500

    threw some boilerplate code in from SOverflow

--- reid_distance_assigner.py
+++ reid_distance_assigner.py
@@ -9,6 +9,26 @@ from __future__ import division
 
 import subprocess
 
+from subprocess import Popen, PIPE, STDOUT
+
+#p = Popen(['grep', 'f'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+#grep_stdout = p.communicate(input='one\ntwo\nthree\nfour\nfive\nsix\n')[0]
+
+#StdinCommand = '''
+#    MolPro code
+#'''
+
 # given some kind of input variables, generate the string or file 
 # that revised_kinematic_distance.f needs
 
+#   name rastring destring vstring fstring
+test_string = "lol    010203.04 121314.5 123 0"
+
+p = Popen(['gfortran', 
+          '/Users/tsrice/Documents/Code/mark_reid_kdist/revised_kinematic_distance_3.f'], 
+          shell = False, stdin = PIPE)
+output, error = p.communicate(input = test_string)
+
+print output
+print error
