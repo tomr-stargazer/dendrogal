9ad04470a9d8442cc7b15d2408ed2a6207fcca8b
Tom Rice <t.rice90@gmail.com>
2014-02-07 00:52:58 -0500

    The demo works!

--- demo.py
+++ demo.py
@@ -11,3 +11,31 @@ import numpy as np
 
 import astropy
 import astrodendro
+import astropy.units as u
+
+from astropy.io.fits import getdata
+
+data_path = "/Users/tsrice/Dropbox/college/Astro99/DATA/"
+
+def cogal_downsampled_demo():
+
+    print "loading data: ..."
+    cogal = getdata(data_path+'COGAL_all_mom.fits', memmap=True)
+
+    cogal_downsampled = cogal[::4, ::4, ::4]
+
+    print "computing dendrogram: ..."
+    d = astrodendro.Dendrogram.compute(
+        cogal_downsampled.transpose(2,0,1), 
+        min_value=0.01, min_delta=0.005, min_npix=30, verbose=True)
+
+    d.viewer(galactic=True)
+
+    metadata = {}
+    metadata['data_unit'] = u.Jy # This is a LIE but Kelvin don't work yet
+
+    catalog = astrodendro.ppv_catalog(d, metadata)
+
+    return d, catalog
+
+    
