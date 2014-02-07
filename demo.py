58e312fa7cd406ed2f26b457e2fdcd86f45e4ed8
Tom Rice <t.rice90@gmail.com>
2014-02-07 00:12:18 -0500

    Made a baby demo.py!

---
+++ demo.py
@@ -0,0 +1,13 @@
+"""
+This is a demo to open, downsample, and dendrogram some sample data.
+
+The data are allowed to be the "actual" thing we're trying to dendrogram I guess.
+
+"""
+
+from __future__ import division
+
+import numpy as np
+
+import astropy
+import astrodendro
