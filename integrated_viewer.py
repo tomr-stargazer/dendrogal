0d600c14a5a1a6a956d13b462fb2b5cba6f2b018
Tom Rice <t.rice90@gmail.com>
2014-03-26 01:03:34 -0400

    Wrote most of the basic plotting code for integrated_viewer!

--- integrated_viewer.py
+++ integrated_viewer.py
@@ -5,36 +5,57 @@ A viewer for astrodendro that looks like an integrated l,b and l,v thing.
 
 from __future__ import division
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 class IntegratedViewer(object):
-    def __init__(self, dendrogram, hub, catalog, xaxis, yaxis):
+    def __init__(self, dendrogram, hub):
+
+        if dendrogram.data.ndim != 3:
+            raise ValueError(
+                "Only 3-dimensional arrays are supported")
 
         self.hub = hub # good
         self.hub.add_callback(self.update_selection) # good
-        self.dendrogram = dendrogram # good 
 
-        self.fig = plt.figure() 
-        self.axes = plt.subplot(1,1,1)
+        self.datacube = dendrogram.data
+        self.dendrogram = dendrogram # good
 
-        self.catalog = catalog
-        self.xdata = catalog[xaxis]
-        self.ydata = catalog[yaxis]
+        self.selected_contour = {} # selection_id -> Contour
 
-        self.x_column_name = xaxis
-        self.y_column_name = yaxis
+        self.fig = plt.figure(figsize=(14, 8))
 
-        self.lines2d = {} # selection_id -> matplotlib.lines.Line2D
+        self.ax1 = self.fig.add_axes([0.1, 0.05, 0.8, 0.4]) # l, b
+        self.ax2 = self.fig.add_axes([0.1, 0.5, 0.8, 0.5]) # l, v
+
+        self.array_lb = np.nansum(self.datacube, axis=0)
+        self.array_lv = np.nansum(self.datacube, axis=1)
 
         self._draw_plot()
         self.hub.add_callback(self.update_selection)
 
     def _draw_plot(self):
+    	""" Create an image to plot things onto. """
 
-        self.axes.plot(self.xdata, self.ydata, 'o', color='w', mec='k', zorder=-5)
+        len_v, len_l, len_b = self.datacube.shape
+        crop_factor = np.round(len_v/5)
 
-        self.axes.set_xlabel(self.x_column_name)
-        self.axes.set_ylabel(self.y_column_name)
+        # Crop l,v plot because it normally goes awkwardly too high and low in v space
+        self.array_lv_cropped = self.array_lv[crop_factor:-crop_factor,:]
+
+        self._clim_lb = (np.min(self.array_lb[~np.isnan(self.array_lb) & ~np.isinf(self.array_lb)]),
+        	             np.max(self.array_lb[~np.isnan(self.array_lb) & ~np.isinf(self.array_lb)]))
+
+        self._clim_lv = (np.min(self.array_lv_cropped[~np.isnan(self.array_lv_cropped) & ~np.isinf(self.array_lv_cropped)]),
+        	             np.max(self.array_lv_cropped[~np.isnan(self.array_lv_cropped) & ~np.isinf(self.array_lv_cropped)]))
+
+        self.image_lb = self.ax1.imshow(self.array_lb, origin='lower', 
+            interpolation='nearest', vmin=self._clim_lb[0], 
+            vmax=0.025*self._clim_lb[1], cmap=plt.cm.gray)
+
+        self.image_lv = self.ax2.imshow(self.array_lv_cropped, origin='lower', 
+            interpolation='nearest', vmin=self._clim_lv[0], 
+            vmax=0.05*self._clim_lv[1], cmap=plt.cm.gray, aspect=2.5)
 
         self.fig.canvas.draw()
 
