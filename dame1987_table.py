fe7ad46f19194f4c4c2eda11e70d4d33e1856f1a
Tom Rice <t.rice90@gmail.com>
2014-06-18 08:43:41 -0400

    Wrote some of the Dame table and a function to convert it into my format.

---
+++ dame1987_table.py
@@ -0,0 +1,68 @@
+"""
+This is a hand-copied version of the Dame 1987 table of molecular clouds
+
+"""
+
+from __future__ import division
+
+import numpy as np
+
+import astropy.units as u
+
+table2 = {
+	'Aquila Rift 1': {'l_min':18.5,
+	                  'l_max':34,
+	                  'b_min':-6,
+	                  'b_max':10,
+	                  'distance':200,
+	                  'mass':1.5},
+	'Aquila Rift 2': {'l_min':34,
+	                  'l_max':44,
+	                  'b_min':-4,
+	                  'b_max':4,
+	                  'distance':200,
+	                  'mass':1.5},
+	'Cloud A': {'l_min':44,
+	            'l_max':49.5,
+	            'b_min':-4,
+	            'b_max':2,
+	            'distance':500,
+	            'mass':0.4},
+	'Cloud B': {'l_min':44,
+	            'l_max':54,
+	            'b_min':-4,
+	            'b_max':5,
+	            'distance':300,
+	            'mass':0.4},
+	'Cloud C': {'l_min':50,
+	            'l_max':55,
+	            'b_min':-1,
+	            'b_max':3.5,
+	            'distance':500,
+	            'mass':0.3},
+	'Taurus':  {'l_min':163,
+	            'l_max':178,
+	            'b_min':-22,
+	            'b_max':-9.5,
+	            'distance':140,
+	            'mass':0.3},
+}
+
+def convert_dame_table_to_standard_form(table):
+
+	for key in table:
+
+		row = table[key]
+
+		radius = np.sqrt((row['l_max']-row['l_min'])*
+			             (row['b_max']-row['b_min']) )
+
+		table[key]['radius'] = radius * u.deg
+
+		l_cen = (row['l_max'] + row['l_min'])/2
+		b_cen = (row['b_max'] + row['b_min'])/2		
+
+		table[key]['l'] = l_cen * u.deg
+		table[key]['b'] = b_cen * u.deg
+
+	return table
