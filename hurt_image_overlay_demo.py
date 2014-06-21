f8b01fe22b8bdee17cd578035a4594f8fbb80616
Tom Rice <t.rice90@gmail.com>
2014-06-21 05:40:33 -0400

    The image underlay thing is way better now.

--- hurt_image_overlay_demo.py
+++ hurt_image_overlay_demo.py
@@ -26,22 +26,28 @@ hurt_image = imread(hurt_image_location)
 # assuming that the galactic center is 8340 pc from the Sun
 # (i.e., disregarding the distance axes on the image itself!)
 
-px_between_sun_and_galactic_center = 115.8
-distance_between_sun_and_galactic_center = 8340 # pc
-
-x_len, y_len, a = np.shape(hurt_image)
-
-image_length_in_pc = x_len * distance_between_sun_and_galactic_center/px_between_sun_and_galactic_center
-
-galactic_x_extent = [image_length_in_pc/2 + distance_between_sun_and_galactic_center,
-	                 -image_length_in_pc/2 + distance_between_sun_and_galactic_center]
-galactic_y_extent = [-image_length_in_pc/2, image_length_in_pc/2]
 
 # galactic_y_extent = [-18378.6, 18269.7] # "y" axis runs through 90 degrees l and 270 degrees l
 # galactic_x_extent = [25057.8, -11329.5] # "x" axis runs through Galactic Center and Anticenter
 
-fig = plt.figure()
-plt.imshow(hurt_image, extent=galactic_y_extent+galactic_x_extent)
+def underlay_hurt_galaxy(fig=None, units=u.pc, **kwargs):
 
-plt.scatter(np.arange(100), 100*np.sin(np.arange(100)/10), marker='o', c='red')
-plt.show()
+	px_between_sun_and_galactic_center = 115.8
+	distance_between_sun_and_galactic_center = 8340 * u.pc
+
+	x_len, y_len, a = np.shape(hurt_image)
+
+	image_length = x_len * distance_between_sun_and_galactic_center/px_between_sun_and_galactic_center
+
+	galactic_x_extent = [image_length/2 + distance_between_sun_and_galactic_center,
+		                 -image_length/2 + distance_between_sun_and_galactic_center]
+	galactic_y_extent = [-image_length/2, image_length/2]
+
+	extents = [element.to(units).value for element in galactic_y_extent+galactic_x_extent]
+
+	fig = fig or plt.figure()
+	plt.imshow(hurt_image, zorder=-10, extent=extents, **kwargs)
+
+	plt.show()
+
+	return fig
