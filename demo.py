ff61eb94d649d50b588f5eacb9b0cf1713e3eba5
Tom Rice <t.rice90@gmail.com>
2014-08-09 13:12:01 -0400

    Refining the wcs_bounds_check to just pix2world. (i haven't tested this)

--- demo.py
+++ demo.py
@@ -208,7 +208,7 @@ def downsampled_demo(data_file, downsample_factor=4, transpose_tuple=(2,0,1),
       downsample_and_transpose_data_and_header(datacube, datacube_header, df, tt, resample=resample, recenter=recenter)
     datacube_dt_wcs = wcs.wcs.WCS(datacube_dt_header)
 
-    datacube_dt_wcs.wcs.bounds_check(False)
+    datacube_dt_wcs.wcs.bounds_check(pix2world=False)
 
     beam_size = 1/8 * u.deg
     frequency = 115 * u.GHz
