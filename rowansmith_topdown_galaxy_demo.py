15addec9aafcad9cd64bea39f56a47f2afedbeee
Tom Rice <t.rice90@gmail.com>
2014-08-14 15:42:21 -0400

    I wrote a quick thing that reproduces the Rowan Smith demo I showed Alyssa.

---
+++ rowansmith_topdown_galaxy_demo.py
@@ -0,0 +1,32 @@
+"""
+This is a demo decomposition of Rowan Smith's simulated top-down CO data image.
+
+"""
+
+from __future__ import division
+
+import pickle
+import os.path
+import datetime
+import numpy as np
+
+import astropy
+import astrodendro
+import astropy.units as u
+import astropy.constants as c
+
+from astropy import wcs
+from astropy.io.fits import getdata, getheader
+import astropy.io.fits as fits
+
+from astrodendro.scatter import Scatter
+from astrodendro_analysis.integrated_viewer import IntegratedViewer
+from astrodendro_analysis.reid_distance_assigner import make_reid_distance_column
+from astrodendro_analysis.assign_physical_values import assign_size_mass_alpha_pressure
+
+data_path = os.path.expanduser("~/Dropbox/DendroGal/")
+
+data, header = getdata(data_path+"WCO.fits", header=True)
+
+d = astrodendro.Dendrogram.compute(data, verbose=True, min_value=1e-2, min_delta=1e-2, min_npix=100)
+
