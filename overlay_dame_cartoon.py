66547aa651bc8d50b7fda394cf3386ac8b09c01e
Tom Rice <t.rice90@gmail.com>
2014-08-14 17:10:29 -0400

    Things are functioning now; it's fairly basic.

--- overlay_dame_cartoon.py
+++ overlay_dame_cartoon.py
@@ -11,23 +11,44 @@ import matplotlib.pyplot as plt
 from scipy.misc import imread
 
 from wcsaxes import WCSAxes
+from astropy import wcs
 
 dropbox_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/")
 
 dame_lb_cartoon_location = dropbox_path+"Fig2_Dame_cropped.png"
 lb_cartoon_image = imread(dame_lb_cartoon_location)
 
-test_fig = plt.figure()
-test_ax = test_fig.add_subplot(111)
+# test_fig = plt.figure()
+# test_ax = test_fig.add_subplot(111)
 
-def overlay_lb_cartoon(ax, wcs):
+# in coordinate values, where are the corners?
+corners = [
+	(206.50549450549448, -49.60396039603961),
+	(206.50549450549448, 44.05940594059403),
+	(-190.87912087912053, 44.05940594059389),
+	(-190.87912087912008, -49.60396039603967)
+	]
 
+px_per_degree = 8.5
 
+def overlay_lb_cartoon(ax, wcs_object=None, **kwargs):
+
+	if wcs_object is None:
+		try:
+			wcs_object= ax.wcs.sub([wcs.WCSSUB_CELESTIAL])
+		except AttributeError:
+			print "`ax` has no `wcs` -- please provide one"
+			return
+
+	left, a = wcs_object.all_world2pix(206.50549450549448-180, 0, 0) 
+	right, a = wcs_object.all_world2pix(-190.87912087912053+360, 0, 0)
+	b, top = wcs_object.all_world2pix(0, 44.05940594059403, 0)
+	b, bottom = wcs_object.all_world2pix(0, -49.60396039603961, 0)
+
+	# extents = [left, right, bottom, top]
+	extents = [-106, 1483.6, bottom, top]
 	
-
-	# 	plt.imshow(hurt_image, zorder=-10, extent=extents, **kwargs)
+	ax.imshow(lb_cartoon_image, zorder=0.05, extent=extents, **kwargs)
 	
-
-	pass
-
+	return extents	
 
