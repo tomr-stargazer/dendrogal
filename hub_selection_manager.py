5de7e4b03756e06ce877211c530c300f15a7a81b
Tom Rice <t.rice90@gmail.com>
2014-08-16 01:00:09 -0400

    Skeleton of hub_selection_manager.py

---
+++ hub_selection_manager.py
@@ -0,0 +1,32 @@
+"""
+Little functions that help manage dendrogram selections.
+
+"""
+
+from __future__ import division
+
+import numpy as np
+
+import astrodendro
+
+# some of these will wanna be function generators
+
+def transfer_function_generator(hub, origin=1, destination=2):
+
+	def transfer_between_selections():
+
+		pass
+
+	return transfer_between_selections
+
+
+def stash_function_generator(hub, origin=2, destination=3):
+
+	def stash_selection():
+
+		pass
+
+	return stash_selection
+
+
+
