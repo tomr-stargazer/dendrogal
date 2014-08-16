186ff95fe3666f98e1d05c8b0a9135f368064c43
Tom Rice <t.rice90@gmail.com>
2014-08-15 23:12:48 -0400

    Cloned all of the DendrogramPlotter functionality into cv

--- integrated_and_cartoon_viewer.py
+++ integrated_and_cartoon_viewer.py
@@ -10,6 +10,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+from astrodendro.plot import DendrogramPlotter
 from wcsaxes import WCSAxes
 
 from overlay_dame_cartoon import overlay_lb_cartoon
@@ -31,12 +32,19 @@ class CartoonDualViewer(object):
         self.dendrogram = dendrogram
         self.wcs = dendrogram.wcs
 
+        self.plotter = DendrogramPlotter(dendrogram)
+        self.plotter.sort(reverse=True)
+
+        # Get the lines as individual elements, and the mapping from line to structure
+        self.lines = self.plotter.get_lines(edgecolor='k')
+
+        self.selected_lines = {}
         self.selected_contours = {} # selection_id -> (contour, contour) tuple
 
         if alignment == 'horizontal':
-            figsize = (11, 9)
-            ax_integrated_limits = [0.05, 0.05, 0.9, 0.5]
-            ax_cartoon_limits = [0.05, 0.55, 0.9, 0.5]
+            figsize = (15, 9)
+            ax_integrated_limits = [0.05, 0.05, 0.66, 0.5]
+            ax_cartoon_limits = [0.05, 0.55, 0.66, 0.5]
         elif alignment == 'vertical':
             figsize = (8, 6)
             ax_integrated_limits = [0.1, 0.1, 0.375, 0.8]
@@ -67,6 +75,33 @@ class CartoonDualViewer(object):
         array_lb[(array_lb < 0) | np.isinf(array_lb) | np.isnan(array_lb)] = 0
         self.array_lb = array_lb
 
+        # all stolen from astrodendro.viewer
+        self.ax_dendrogram = self.fig.add_axes([0.75, 0.1, 0.25, 0.6])
+        self.ax_dendrogram.add_collection(self.lines)
+
+        self.selected_label = {} # map selection IDs -> text objects
+        self.selected_label[1] = self.fig.text(0.75, 0.9, "No structure selected", fontsize=18, 
+            color=self.hub.colors[1])
+        self.selected_label[2] = self.fig.text(0.75, 0.85, "No structure selected", fontsize=18,
+            color=self.hub.colors[2])
+        self.selected_label[3] = self.fig.text(0.75, 0.8, "No structure selected", fontsize=18,
+            color=self.hub.colors[3])
+        x = [p.vertices[:, 0] for p in self.lines.get_paths()]
+        y = [p.vertices[:, 1] for p in self.lines.get_paths()]
+        xmin = np.min(x)
+        xmax = np.max(x)
+        ymin = np.min(y)
+        ymax = np.max(y)
+        self.lines.set_picker(2.)
+        self.lines.set_zorder(0)
+        dx = xmax - xmin
+        self.ax_dendrogram.set_xlim(xmin - dx * 0.1, xmax + dx * 0.1)
+        self.ax_dendrogram.set_ylim(ymin * 0.5, ymax * 2.0)
+        self.ax_dendrogram.set_yscale('log')
+
+        self.fig.canvas.mpl_connect('pick_event', self.line_picker)
+
+
         # array_lv = np.nansum(self.datacube, axis=1)
         # array_lv[(array_lv < 0) | np.isinf(array_lv) | np.isnan(array_lv)] = 0
         # self.array_lv = array_lv
@@ -81,6 +116,7 @@ class CartoonDualViewer(object):
 
         self._draw_plot()
         self.hub.add_callback(self.update_selection)
+        self.hub.add_callback(self._update_lines)        
         self.fig.canvas.mpl_connect('button_press_event', self.select_from_map)
 
         # If things are already selected in the hub, go select them!
@@ -174,3 +210,69 @@ class CartoonDualViewer(object):
                         linewidths=5, levels=[0.5], alpha=0.9, zorder=struct.height) )
 
         self.fig.canvas.draw()
+
+
+    def line_picker(self, event):
+
+    	# Only do this if no tools are currently selected
+    	if event.canvas.toolbar.mode != '':
+    	    return
+    	if event.mouseevent.button not in self.selected_label:
+    	    return
+
+    	input_key = event.mouseevent.button
+
+    	# event.ind gives the indices of the paths that have been selected
+
+    	# Find levels of selected paths
+    	peaks = [event.artist.structures[i].get_peak(subtree=True)[1] for i in event.ind]
+
+    	# Find position of minimum level (may be duplicates, let Numpy decide)
+    	ind = event.ind[np.argmax(peaks)]
+
+    	# Extract structure
+    	structure = event.artist.structures[ind]
+
+    	# Select the structure
+    	self.hub.select(input_key, structure)
+
+    	# Re-draw
+    	event.canvas.draw()
+
+    def _update_lines(self, selection_id):
+    	structures = self.hub.selections[selection_id]
+    	select_subtree = self.hub.select_subtree[selection_id]
+
+    	structure = structures[0]
+
+    	# Remove previously selected collection
+    	if selection_id in self.selected_lines:
+    	    self.ax_dendrogram.collections.remove(self.selected_lines[selection_id])
+    	    del self.selected_lines[selection_id]
+
+    	if structure is None:
+    	    self.selected_label[selection_id].set_text("No structure selected")
+    	    self.remove_contour(selection_id)
+    	    self.fig.canvas.draw()
+    	    return
+
+    	# self.remove_all_contours()
+
+    	if len(structures) <= 1:
+    	    label_text = "Selected structure: {0}".format(structure.idx)
+    	elif len(structures) <=3:
+    	    label_text = "Selected structures: {0}".format(', '.join([str(structure.idx) for structure in structures]))
+    	else:
+    	    label_text = "Selected structures: {0}...".format(', '.join([str(structure.idx) for structure in structures[:3]]))
+
+    	self.selected_label[selection_id].set_text(label_text)
+
+    	# Get collection for this substructure
+    	self.selected_lines[selection_id] = self.plotter.get_lines(
+    	    structures=structures, subtree=select_subtree)
+    	self.selected_lines[selection_id].set_color(self.hub.colors[selection_id])
+    	self.selected_lines[selection_id].set_linewidth(2)
+    	self.selected_lines[selection_id].set_zorder(structure.height)
+
+    	# Add to axes
+    	self.ax_dendrogram.add_collection(self.selected_lines[selection_id])
