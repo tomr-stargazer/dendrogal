b69f91387a73b3d9fe9049d59b98cf5e44003507
Tom Rice <t.rice90@gmail.com>
2014-08-16 03:03:27 -0400

    Improved & debugged most of this!

--- hub_selection_manager.py
+++ hub_selection_manager.py
@@ -5,6 +5,8 @@ Little functions that help manage dendrogram selections.
 
 from __future__ import division
 
+import random
+
 import numpy as np
 
 import astrodendro
@@ -15,15 +17,19 @@ def transfer_function_generator(hub, origin=1, destination=2):
 
     def transfer_between_selections():
 
-        hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
+        try:
+            joint_selection = set([x for x in hub.selections[origin]+hub.selections[destination] if x is not None])
+            hub.select(destination, list(joint_selection), subtree=False)
+        except KeyError:
+            hub.select(destination, hub.selections[origin], subtree=False)            
         hub.select(origin, None)
 
     return transfer_between_selections
 
-selection_dictionary = {}
-selection_ID_dictionary = {}
-
 def stash_function_generator(hub, origin=2, destination=3):
+
+    selection_dictionary = {}
+    selection_ID_dictionary = {}
 
     def stash_selection(name):
 
@@ -36,8 +42,13 @@ def stash_function_generator(hub, origin=2, destination=3):
             selection_dictionary[name] = hub.selections[origin]
             selection_ID_dictionary[name] = len(selection_dictionary.keys()) # increment by one
 
-        hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
+        try:
+            joint_selection = [x for x in hub.selections[origin]+hub.selections[destination] if x is not None]
+            hub.select(destination, joint_selection, subtree=False)
+        except KeyError:
+            hub.select(destination, hub.selections[origin], subtree=False)            
         hub.select(origin, None)
 
-    return stash_selection
+    return stash_selection, selection_dictionary, selection_ID_dictionary
+
 
