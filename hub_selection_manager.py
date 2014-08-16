515f4037b659316dac01210801bfd6fe774c6836
Tom Rice <t.rice90@gmail.com>
2014-08-16 01:28:10 -0400

    Fleshed out the manager functions a bit!

--- hub_selection_manager.py
+++ hub_selection_manager.py
@@ -15,18 +15,29 @@ def transfer_function_generator(hub, origin=1, destination=2):
 
 	def transfer_between_selections():
 
-		pass
+		hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
+		hub.select(origin, None)
 
 	return transfer_between_selections
 
+selection_dictionary = {}
+selection_ID_dictionary = {}
 
 def stash_function_generator(hub, origin=2, destination=3):
 
-	def stash_selection():
+	def stash_selection(name):
 
-		pass
+		if type(name) is not str:
+			raise ValueError("`name` must be a string, e.g. 'Orion'")
+
+		try:
+			selection_dictionary[name] += hub.selections[origin]
+		except KeyError:
+			selection_dictionary[name] = hub.selections[origin]
+			selection_ID_dictionary[name] = len(selection_dictionary.keys()) # increment by one
+
+		hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
+		hub.select(origin, None)
 
 	return stash_selection
-
-
 
