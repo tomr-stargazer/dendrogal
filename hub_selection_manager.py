513d78577e2f11026c16cd65b9810cec4b53344e
Tom Rice <t.rice90@gmail.com>
2014-08-16 01:42:36 -0400

    Indent using spaces.

--- hub_selection_manager.py
+++ hub_selection_manager.py
@@ -13,31 +13,31 @@ import astrodendro
 
 def transfer_function_generator(hub, origin=1, destination=2):
 
-	def transfer_between_selections():
+    def transfer_between_selections():
 
-		hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
-		hub.select(origin, None)
+        hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
+        hub.select(origin, None)
 
-	return transfer_between_selections
+    return transfer_between_selections
 
 selection_dictionary = {}
 selection_ID_dictionary = {}
 
 def stash_function_generator(hub, origin=2, destination=3):
 
-	def stash_selection(name):
+    def stash_selection(name):
 
-		if type(name) is not str:
-			raise ValueError("`name` must be a string, e.g. 'Orion'")
+        if type(name) is not str:
+            raise ValueError("`name` must be a string, e.g. 'Orion'")
 
-		try:
-			selection_dictionary[name] += hub.selections[origin]
-		except KeyError:
-			selection_dictionary[name] = hub.selections[origin]
-			selection_ID_dictionary[name] = len(selection_dictionary.keys()) # increment by one
+        try:
+            selection_dictionary[name] += hub.selections[origin]
+        except KeyError:
+            selection_dictionary[name] = hub.selections[origin]
+            selection_ID_dictionary[name] = len(selection_dictionary.keys()) # increment by one
 
-		hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
-		hub.select(origin, None)
+        hub.select(destination, hub.selections[origin]+hub.selections[destination], subtree=False)
+        hub.select(origin, None)
 
-	return stash_selection
+    return stash_selection
 
