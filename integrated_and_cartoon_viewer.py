dabeb6f608996e551e3cc6b3e6bc0856868712cb
Tom Rice <t.rice90@gmail.com>
2014-08-18 17:02:50 -0400

    Now the cartoon viewer has multipurpose

--- integrated_and_cartoon_viewer.py
+++ integrated_and_cartoon_viewer.py
@@ -13,10 +13,10 @@ import matplotlib.pyplot as plt
 from astrodendro.plot import DendrogramPlotter
 from wcsaxes import WCSAxes
 
-from overlay_dame_cartoon import overlay_lb_cartoon
+from overlay_dame_cartoon import overlay_lb_cartoon, overlay_lv_cartoon
 
 class CartoonDualViewer(object):
-    def __init__(self, dendrogram, hub, alignment='horizontal', cmap=plt.cm.gray,
+    def __init__(self, dendrogram, hub, alignment='latitude', cmap=plt.cm.gray,
                  clip_velocity=None):
 
         if dendrogram.data.ndim != 3:
@@ -41,39 +41,51 @@ class CartoonDualViewer(object):
         self.selected_lines = {}
         self.selected_contours = {} # selection_id -> (contour, contour) tuple
 
-        if alignment == 'horizontal':
+        if alignment == 'latitude':
             figsize = (15, 9)
             ax_integrated_limits = [0.025, 0.05, 0.6, 0.5]
             ax_cartoon_limits = [0.025, 0.55, 0.6, 0.5]
-        elif alignment == 'vertical':
-            figsize = (8, 6)
-            ax_integrated_limits = [0.1, 0.1, 0.375, 0.8]
-            ax_cartoon_limits = [0.55, 0.1, 0.375, 0.8]
+
+            slices = ('x', 'y', 0)
+            self.sum_axis=0
+            self.overlay_cartoon = overlay_lb_cartoon
+            self.aspect = 1
+        elif alignment == 'velocity':
+            figsize = (15, 9)
+            ax_integrated_limits = [0.025, 0.05, 0.6, 0.5]
+            ax_cartoon_limits = [0.025, 0.55, 0.6, 0.5]
+
+            slices = ('x', 0, 'y')
+            self.sum_axis=1
+            self.overlay_cartoon = overlay_lv_cartoon
+            self.aspect = 2.5
         # elif alignment == 'pp' or alignment == 'lb':
         #     figsize = (10, 4)
         #     ax_integrated_limits = [0.1, 0.1, 0.8, 0.8]
         #     ax_cartoon_limits = [0,0,0.01,0.01]
         else:
-            raise ValueError("`alignment` must be 'horizontal' or 'vertical'")
+            raise ValueError("`alignment` must be 'latitude' or 'velocity'")
+
+        self.alignment = alignment
 
         self.fig = plt.figure(figsize=figsize)
         self.cmap = cmap
 
         if self.dendrogram.wcs is not None:
-            ax_integrated = WCSAxes(self.fig, ax_integrated_limits, wcs=self.dendrogram.wcs, slices=('x', 'y', 0))
+            ax_integrated = WCSAxes(self.fig, ax_integrated_limits, wcs=self.dendrogram.wcs, slices=slices)
             self.ax_integrated = self.fig.add_axes(ax_integrated)
 
-            ax_cartoon = WCSAxes(self.fig, ax_cartoon_limits, wcs=self.dendrogram.wcs, slices=('x', 'y', 0), 
-            	sharex=ax_integrated, sharey=ax_integrated)
+            ax_cartoon = WCSAxes(self.fig, ax_cartoon_limits, wcs=self.dendrogram.wcs, slices=slices, 
+                sharex=ax_integrated, sharey=ax_integrated)
             self.ax_cartoon = self.fig.add_axes(ax_cartoon)
         else:
             self.ax_integrated = self.fig.add_axes(ax_integrated_limits)
             self.ax_cartoon = self.fig.add_axes(ax_cartoon_limits,
-            	sharex=ax_integrated, sharey=ax_integrated)
+                sharex=ax_integrated, sharey=ax_integrated)
 
-        array_lb = np.nansum(self.datacube, axis=0)
-        array_lb[(array_lb < 0) | np.isinf(array_lb) | np.isnan(array_lb)] = 0
-        self.array_lb = array_lb
+        array_summed = np.nansum(self.datacube, axis=self.sum_axis)
+        array_summed[(array_summed < 0) | np.isinf(array_summed) | np.isnan(array_summed)] = 0
+        self.array_summed = array_summed
 
         # all stolen from astrodendro.viewer
         self.ax_dendrogram = self.fig.add_axes([0.65, 0.1, 0.35, 0.6])
@@ -124,14 +136,14 @@ class CartoonDualViewer(object):
             self.update_selection(selection_id)
 
     def _draw_plot(self):
-    	""" Create an image to plot things onto. """
+        """ Create an image to plot things onto. """
 
         len_v, len_l, len_b = self.datacube.shape
 
-        self.image_integrated = self.ax_integrated.imshow(np.log10(self.array_lb+1), origin='lower', 
-            interpolation='nearest', cmap=self.cmap)
+        self.image_cartoon = self.overlay_cartoon(self.ax_cartoon)[0]
 
-        self.image_cartoon = overlay_lb_cartoon(self.ax_cartoon)[0]
+        self.image_integrated = self.ax_integrated.imshow(np.log10(self.array_summed+1), origin='lower', 
+            interpolation='nearest', cmap=self.cmap, aspect=self.aspect)
 
         # Trim the top and bottom of the l, v plot for cosmetic reasons
         # if self.clip_velocity:
@@ -159,14 +171,28 @@ class CartoonDualViewer(object):
 
         if event.inaxes is self.ax_integrated or event.inaxes is self.ax_cartoon:
 
-            # Find pixel co-ordinates of click
-            ix = int(round(event.xdata))
-            iy = int(round(event.ydata))
+            if self.alignment == 'latitude':
 
-            try:
-                iz = np.where(self.datacube[:, iy, ix] == np.nanmax(self.datacube[:, iy, ix]))[0][0]
-            except IndexError:
-                iz = 0
+                # Find pixel co-ordinates of click
+                ix = int(round(event.xdata))
+                iy = int(round(event.ydata))
+
+                try:
+                    iz = np.where(self.datacube[:, iy, ix] == np.nanmax(self.datacube[:, iy, ix]))[0][0]
+                except IndexError:
+                    iz = 0
+
+            elif self.alignment == 'velocity':
+
+                # Find pixel co-ordinates of click
+                ix = int(round(event.xdata))
+                iz = int(round(event.ydata))
+
+                try:
+                    iy = np.where(self.datacube[iz, :, ix] == np.nanmax(self.datacube[iz, :, ix]))[0][0]
+                except IndexError:
+                    iy = 0
+
         else:
             return
 
@@ -198,15 +224,14 @@ class CartoonDualViewer(object):
                 else:
                     mask = reduce(np.add, [structure.get_mask(subtree=True) for structure in structures])
                 # numpy magic sets nonzero values to True
-                mask_lb = np.sum(mask, axis=0).astype('bool') 
-                mask_lv = np.sum(mask, axis=1).astype('bool')
+                mask_summed = np.sum(mask, axis=self.sum_axis).astype('bool') 
 
                 self.selected_contours[selection_id] = (
                     self.ax_integrated.contour(
-                        mask_lb, colors=self.hub.colors[selection_id],
+                        mask_summed, colors=self.hub.colors[selection_id],
                         linewidths=5, levels=[0.5], alpha=0.9, zorder=struct.height), 
                     self.ax_cartoon.contour(
-                        mask_lb, colors=self.hub.colors[selection_id],
+                        mask_summed, colors=self.hub.colors[selection_id],
                         linewidths=5, levels=[0.5], alpha=0.9, zorder=struct.height) )
 
         self.fig.canvas.draw()
@@ -214,65 +239,65 @@ class CartoonDualViewer(object):
 
     def line_picker(self, event):
 
-    	# Only do this if no tools are currently selected
-    	if event.canvas.toolbar.mode != '':
-    	    return
-    	if event.mouseevent.button not in self.selected_label:
-    	    return
+        # Only do this if no tools are currently selected
+        if event.canvas.toolbar.mode != '':
+            return
+        if event.mouseevent.button not in self.selected_label:
+            return
 
-    	input_key = event.mouseevent.button
+        input_key = event.mouseevent.button
 
-    	# event.ind gives the indices of the paths that have been selected
+        # event.ind gives the indices of the paths that have been selected
 
-    	# Find levels of selected paths
-    	peaks = [event.artist.structures[i].get_peak(subtree=True)[1] for i in event.ind]
+        # Find levels of selected paths
+        peaks = [event.artist.structures[i].get_peak(subtree=True)[1] for i in event.ind]
 
-    	# Find position of minimum level (may be duplicates, let Numpy decide)
-    	ind = event.ind[np.argmax(peaks)]
+        # Find position of minimum level (may be duplicates, let Numpy decide)
+        ind = event.ind[np.argmax(peaks)]
 
-    	# Extract structure
-    	structure = event.artist.structures[ind]
+        # Extract structure
+        structure = event.artist.structures[ind]
 
-    	# Select the structure
-    	self.hub.select(input_key, structure)
+        # Select the structure
+        self.hub.select(input_key, structure)
 
-    	# Re-draw
-    	event.canvas.draw()
+        # Re-draw
+        event.canvas.draw()
 
     def _update_lines(self, selection_id):
-    	structures = self.hub.selections[selection_id]
-    	select_subtree = self.hub.select_subtree[selection_id]
+        structures = self.hub.selections[selection_id]
+        select_subtree = self.hub.select_subtree[selection_id]
 
-    	structure = structures[0]
+        structure = structures[0]
 
-    	# Remove previously selected collection
-    	if selection_id in self.selected_lines:
-    	    self.ax_dendrogram.collections.remove(self.selected_lines[selection_id])
-    	    del self.selected_lines[selection_id]
+        # Remove previously selected collection
+        if selection_id in self.selected_lines:
+            self.ax_dendrogram.collections.remove(self.selected_lines[selection_id])
+            del self.selected_lines[selection_id]
 
-    	if structure is None:
-    	    self.selected_label[selection_id].set_text("No structure selected")
-    	    self.remove_contour(selection_id)
-    	    self.fig.canvas.draw()
-    	    return
+        if structure is None:
+            self.selected_label[selection_id].set_text("No structure selected")
+            self.remove_contour(selection_id)
+            self.fig.canvas.draw()
+            return
 
-    	# self.remove_all_contours()
+        # self.remove_all_contours()
 
-    	if len(structures) <= 1:
-    	    label_text = "Selected structure: {0}".format(structure.idx)
-    	elif len(structures) <=3:
-    	    label_text = "Selected structures: {0}".format(', '.join([str(structure.idx) for structure in structures]))
-    	else:
-    	    label_text = "Selected structures: {0}...".format(', '.join([str(structure.idx) for structure in structures[:3]]))
+        if len(structures) <= 1:
+            label_text = "Selected structure: {0}".format(structure.idx)
+        elif len(structures) <=3:
+            label_text = "Selected structures: {0}".format(', '.join([str(structure.idx) for structure in structures]))
+        else:
+            label_text = "Selected structures: {0}...".format(', '.join([str(structure.idx) for structure in structures[:3]]))
 
-    	self.selected_label[selection_id].set_text(label_text)
+        self.selected_label[selection_id].set_text(label_text)
 
-    	# Get collection for this substructure
-    	self.selected_lines[selection_id] = self.plotter.get_lines(
-    	    structures=structures, subtree=select_subtree)
-    	self.selected_lines[selection_id].set_color(self.hub.colors[selection_id])
-    	self.selected_lines[selection_id].set_linewidth(2)
-    	self.selected_lines[selection_id].set_zorder(structure.height)
+        # Get collection for this substructure
+        self.selected_lines[selection_id] = self.plotter.get_lines(
+            structures=structures, subtree=select_subtree)
+        self.selected_lines[selection_id].set_color(self.hub.colors[selection_id])
+        self.selected_lines[selection_id].set_linewidth(2)
+        self.selected_lines[selection_id].set_zorder(structure.height)
 
-    	# Add to axes
-    	self.ax_dendrogram.add_collection(self.selected_lines[selection_id])
+        # Add to axes
+        self.ax_dendrogram.add_collection(self.selected_lines[selection_id])
