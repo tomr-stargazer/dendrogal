c0abc26a9dce7921efb697995fc262a79eb9b186
Tom Rice <t.rice90@gmail.com>
2014-09-21 23:10:29 -0400

    Added a little bit to the baby Orion analysis

--- wilson_orion_analysis.py
+++ wilson_orion_analysis.py
@@ -4,3 +4,13 @@ An analysis & catalog production of the Orion region using Wilson 2005 as a base
 """
 
 from __future__ import division
+
+import astropy.units as u
+import astropy.constants as c
+
+from demo import orion_demo
+# def orion_demo(**kwargs):
+#     return downsampled_demo('DHT27_Orion_mom.fits', **kwargs)
+
+d, catalog, x, y = orion_demo(downsample_factor=2, resample=True, recenter=False, min_npix=10, min_value=0.1, min_delta=0.1)
+
