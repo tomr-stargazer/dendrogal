4a5d8a6af12eebe81b64a6f57c6dd3f8cd8a7eba
Tom Rice <t.rice90@gmail.com>
2014-09-23 13:04:52 -0400

    added firstquad demo

--- demo.py
+++ demo.py
@@ -196,6 +196,9 @@ def polaris_demo(**kwargs):
 def ophiuchus_demo(**kwargs):
     return downsampled_demo("DHT37_Ophiuchus_mom.fits", **kwargs)
 
+def firstquad_demo(**kwargs):
+    return downsampled_demo("DHT08_Quad1_mom.fits", **kwargs)
+
 def downsampled_demo(data_file, downsample_factor=4, transpose_tuple=(2,0,1),
                      min_value=0.01, min_delta=0.005, min_npix=2000, 
                      neighbours=None, resample=False, compute_catalog=True, recenter=True):
