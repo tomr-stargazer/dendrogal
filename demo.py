d467d316daef74c5d42c13d82236b3d668ec6314
Tom Rice <t.rice90@gmail.com>
2014-09-28 22:10:24 -0400

    Added secondquad_demo

--- demo.py
+++ demo.py
@@ -199,6 +199,9 @@ def ophiuchus_demo(**kwargs):
 def firstquad_demo(**kwargs):
     return downsampled_demo("DHT08_Quad1_mom.fits", **kwargs)
 
+def secondquad_demo(**kwargs):
+    return downsampled_demo("DHT17_Quad2_bw_mom.fits", **kwargs)
+
 def downsampled_demo(data_file, downsample_factor=4, transpose_tuple=(2,0,1),
                      min_value=0.01, min_delta=0.005, min_npix=2000, 
                      neighbours=None, resample=False, compute_catalog=True, recenter=True):
