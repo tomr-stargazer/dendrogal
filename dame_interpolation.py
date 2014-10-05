0ad20879b2228f584f35510538bdd0a62e37b962
Tom Rice <t.rice90@gmail.com>
2014-10-05 14:44:25 -0400

    null commit for dame_interpolation

---
+++ dame_interpolation.py
@@ -0,0 +1,11 @@
+""" 
+This is an implementation of Tom Dame's datacube interpolation technique. 
+
+"In each spectrum, <= 2 missing channels are filled by linear interpolation. 
+In each spatial plane, single missing pixels are filled by linear 
+interpolation, first in l direction, then b."
+
+http://www.cfa.harvard.edu/rtdc/CO/CompositeSurveys/
+
+"""
+
