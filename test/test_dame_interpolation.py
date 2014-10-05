1c2d66951628ced5006f010d72d412dc114962c5
Tom Rice <t.rice90@gmail.com>
2014-10-05 17:08:47 -0400

    Tests are designed as desired, and passing.

--- test/test_dame_interpolation.py
+++ test/test_dame_interpolation.py
@@ -10,13 +10,66 @@ from __future__ import division
 import numpy as np
 from numpy.testing import assert_allclose
 
-from ..dame_interpolation import interpolate_spectrum
+from ..dame_interpolation import interpolate_spectrum, interpolate_single, interpolate_double, interpolate_datacube
 
-def test_interpolate_spectrum():
+def test_interpolate_single():
 
-	expected = np.arange(10)
+	expected = np.arange(10, dtype='float')
+	expected[0] = np.nan
 
 	data = np.copy(expected)
 	data[5] = np.nan
 
-	assert_allclose(data, expected)
+	assert_allclose(interpolate_single(data), expected)
+
+def test_interpolate_double():
+
+	expected = np.arange(10, dtype='float')
+	expected[0] = np.nan
+
+	data = np.copy(expected)
+	data[5] = np.nan
+	data[6] = np.nan
+
+	assert_allclose(interpolate_double(data), expected)
+
+def test_interpolate_spectrum():
+
+	expected = np.arange(10, dtype='float')
+	expected[0] = np.nan
+
+	data = np.copy(expected)
+	data[5] = np.nan
+	data[6] = np.nan
+
+	data[8] = np.nan
+
+	assert_allclose(interpolate_spectrum(data), expected)
+
+def test_interpolate_datacube():
+
+	expected = np.arange(60, dtype='float').reshape(5,4,3)
+	# should not be interpolated ever
+	expected[0,0,0] = np.nan
+
+	data = np.copy(expected)
+
+	# these should be interpolated velocity-wise
+	data[2,1,1] = np.nan
+	data[3,1,1] = np.nan
+
+	data[1,2,0] = np.nan
+
+	# these should be interpolated spatially
+	data[4,2,2] = np.nan
+
+	data[0,3,1] = np.nan
+
+	data[0,2,0] = np.nan
+
+	data[2,2,1] = np.nan # will only work if velocity gets interpolated first	
+
+	assert_allclose(interpolate_datacube(data), expected)
+
+
+
