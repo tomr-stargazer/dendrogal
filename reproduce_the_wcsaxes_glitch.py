3299614560c555b6115a1224caf55995006d65e2
Tom Rice <t.rice90@gmail.com>
2014-10-08 09:46:57 -0400

    Made the script self-contained

--- reproduce_the_wcsaxes_glitch.py
+++ reproduce_the_wcsaxes_glitch.py
@@ -12,7 +12,75 @@ from astropy import wcs
 from astropy.io.fits import getdata
 
 from wcsaxes import WCSAxes
-from demo import downsample_and_transpose_data_and_header
+
+def downsample_and_transpose_data_and_header(input_data, input_header, 
+                                             downsample_factor=4, 
+                                             transpose_tuple=(2,0,1), resample=False, recenter=True):
+    
+    df = downsample_factor
+    tt = transpose_tuple
+
+    if resample and df > 1:
+        new_data = resample_3d(input_data, df).transpose(*tt)
+    else:
+        # Someday I may improve this with something less crude.
+        new_data = input_data[::df, ::df, ::df].transpose(*tt)
+
+    new_header = input_header.copy()
+
+    # let's transpose. and downsample.
+    new_header['naxis'+str(tt[0]+1)] = input_header['naxis1'] // df
+    new_header['naxis'+str(tt[1]+1)] = input_header['naxis2'] // df
+    new_header['naxis'+str(tt[2]+1)] = input_header['naxis3'] // df    
+
+    new_header['ctype'+str(tt[0]+1)] = input_header['ctype1']
+    new_header['ctype'+str(tt[1]+1)] = input_header['ctype2']
+    new_header['ctype'+str(tt[2]+1)] = input_header['ctype3']
+
+    new_header['crval'+str(tt[0]+1)] = input_header['crval1']
+    new_header['crval'+str(tt[1]+1)] = input_header['crval2']
+    new_header['crval'+str(tt[2]+1)] = input_header['crval3']
+
+    new_header['cdelt'+str(tt[0]+1)] = input_header['cdelt1'] * df
+    new_header['cdelt'+str(tt[1]+1)] = input_header['cdelt2'] * df
+    new_header['cdelt'+str(tt[2]+1)] = input_header['cdelt3'] * df
+
+    if resample:
+        new_header['crpix'+str(tt[0]+1)] = (input_header['crpix1'] - 1)//df
+        new_header['crpix'+str(tt[1]+1)] = (input_header['crpix2'] - 1)//df
+        new_header['crpix'+str(tt[2]+1)] = (input_header['crpix3'] - 1)//df
+    else:
+        new_header['crpix'+str(tt[0]+1)] = (input_header['crpix1'] - 1)//df + 1
+        new_header['crpix'+str(tt[1]+1)] = (input_header['crpix2'] - 1)//df + 1
+        new_header['crpix'+str(tt[2]+1)] = (input_header['crpix3'] - 1)//df + 1
+
+    if recenter:
+        return_header = recenter_wcs_header(new_header)
+    else:
+        return_header = new_header
+
+    return new_data, return_header
+
+def recenter_wcs_header(input_header, central_value=0):
+    """ 
+    Sets the header CRVAL on zero if it's not already. 
+
+    A quick-and-dirty implementation.
+
+    """
+
+    new_header = input_header.copy()
+
+    # in principle we could iterate through dimensions but.... not yet
+    if input_header['CRVAL1'] != central_value:
+        # how do we figure out where it hits zero? Use CDELT1 and do some math?
+        degrees_correction = central_value - input_header['CRVAL1']
+        pixels_correction = degrees_correction / input_header['CDELT1']
+
+        new_header['CRVAL1'] = central_value
+        new_header['CRPIX1'] = input_header['CRPIX1'] + pixels_correction
+
+    return new_header
 
 data_path = os.path.expanduser("~/Dropbox/College/Astro99/DATA/")
 
