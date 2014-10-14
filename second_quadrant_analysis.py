726cf7016cd7b3df14b512912f54b956f21b3a70
Tom Rice <t.rice90@gmail.com>
2014-10-14 11:11:24 -0400

    Added a third-quad analysis with similar parameters

--- second_quadrant_analysis.py
+++ second_quadrant_analysis.py
@@ -55,6 +55,60 @@ def secondquad_distance_demo(downsample=1, distance='reid', nearfar='near', min_
                     (catalog['v_rms'] > 30) | 
                     (catalog['size'] > 1000) |
                     (np.abs(catalog['v_cen']) < 13) |
+                    (np.abs(catalog['x_cen'] - 180) < 10) |
+                    (catalog['area_exact'] > 50) )
+
+    catalog['Distance'][disqualified] = np.nan
+    catalog['v_rms'][disqualified] = np.nan
+    catalog['size'][disqualified] = np.nan
+    catalog['virial'][disqualified] = np.nan
+    catalog['mass'][disqualified] = np.nan
+
+
+    print("\n# Run these commands:\n"
+        "# (these assume you have called this function like following: )\n"
+        "# d, catalog, x, y = near_galaxy_distance_demo(resample=2) \n"
+        "dv = d.viewer()\n"
+        "iv = IntegratedViewer(d, dv.hub, wcs=y['wcs'].sub([wcs.WCSSUB_CELESTIAL]), cmap='gray_r')\n"
+        "dsd = Scatter(d, dv.hub, catalog, 'y_galactocentric', 'x_galactocentric')")
+
+    return d, catalog, header, metadata
+
+
+def thirdquad_distance_demo(downsample=1, distance='reid', nearfar='near', min_npix=20, min_value=0.05, min_delta=0.3, **kwargs):
+
+    d, catalog, header, metadata = demo.thirdquad_demo_mominterp(downsample_factor=downsample, 
+        min_npix=min_npix, min_value=min_value, min_delta=min_delta, recenter=False, **kwargs)
+
+    if distance != 'reid':
+        blitz = make_blitz_distance_column(catalog)
+        catalog['Distance'] = blitz
+    else:
+        reid_distance = make_reid_distance_column(catalog, nearfar=nearfar)
+        catalog['Distance'] = reid_distance['D_k']
+
+    x, y, z = assign_galactocentric_coordinates(catalog, galactic_center_distance=0)
+
+    catalog['x_galactocentric'] = x
+    catalog['y_galactocentric'] = y
+    catalog['z_galactocentric'] = z
+
+    s, m, v, p = assign_size_mass_alpha_pressure(catalog)
+
+    catalog['size'] = s
+    catalog['mass'] = m
+    catalog['virial'] = v
+    catalog['pressure'] = p
+
+    # disqualify!
+
+    disqualified = ((catalog['mass'] > 1e8) | 
+                    (catalog['mass'] < 5e3) | 
+                    # (catalog['major_sigma'] > 10) | 
+                    # (catalog['v_rms'] > 30) | 
+                    # (catalog['size'] > 1000) |
+                    # (np.abs(catalog['v_cen']) < 13) |
+                    # (np.abs(catalog['x_cen'] - 180) < 10) |
                     (catalog['area_exact'] > 50) )
 
     catalog['Distance'][disqualified] = np.nan
