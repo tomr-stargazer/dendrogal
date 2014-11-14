45472e488efd08b6b26ee627d338e00bba15893d
Tom Rice <t.rice90@gmail.com>
2014-11-14 17:04:35 -0500

    added more to the basics

--- fcrao_demo.py
+++ fcrao_demo.py
@@ -15,3 +15,15 @@ import astropy.constants as c
 from astropy import wcs
 from astropy.io.fits import getdata, getheader
 import astropy.io.fits as fits
+
+data_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/FCRAO_data/")
+
+# test_load 
+
+def test_load(data_file="grs-35-cube.fits"):
+
+    datacube, datacube_header = getdata(data_path+data_file, memmap=True,
+                                        header=True)
+
+    return datacube, datacube_header
+
