ec9a4587ead3ca5dcb20451087c1bbea66581cf1
Tom Rice <t.rice90@gmail.com>
2014-11-21 03:36:20 -0500

    started working on the momentmask experiment.

--- orion_many_distance_analysis.py
+++ orion_many_distance_analysis.py
@@ -20,7 +20,10 @@ from astropy import wcs
 from astropy.io.fits import getdata, getheader
 import astropy.io.fits as fits
 
-from demo import resample_3d_variable, downsampled_demo
+from FITS_tools.cube_regrid import gsmooth_cube
+
+from demo import resample_3d_variable, downsampled_demo, downsample_and_transpose_data_and_header
+from dame_moment_masking import moment_mask
 
 data_path = os.path.expanduser("~/Dropbox/College/Astro99/DATA/")
 
@@ -40,12 +43,13 @@ def test_orion(data_file=data_file, distance_multiplier=1, **kwargs):
 # then you downsample in various ways, add noise at each "distance", measure what you get out!
 # using the dendrogram extraction of course (:
 
-
 def determine_number_of_structures_as_a_function_of_distance():
 
-    dm_array = np.arange(20)+1
+    dm_array = np.arange(19)+2
 
     n_structs = []
+
+    max_v_rms = []
 
     for i in dm_array:
 
@@ -53,5 +57,31 @@ def determine_number_of_structures_as_a_function_of_distance():
 
         n_structs.append(len(catalog))
 
-    return dm_array, n_structs
+        max_v_rms.append(catalog['v_rms'][catalog['radius'] ==max(catalog['radius'])][0])
+
+    return dm_array, n_structs, max_v_rms
+
+
+
+def downsample_addnoise_and_momentmask(datacube, header, downsample_factor, rms_noise):
+
+    smooth_cube = gsmooth_cube(datacube, [downsample_factor, downsample_factor, 1], kernelsize_mult=1)
+
+    downsample_tuple = (downsample_factor, downsample_factor, 1)
+
+    downsampled_datacube, downsampled_header = downsample_and_transpose_data_and_header(smooth_cube, header, downsample_factor=downsample_tuple, resample=True)
+
+    noise_cube = np.random.standard_normal(downsampled_datacube.shape) * rms_noise
+
+    noisy_cube = downsampled_datacube + noise_cube
+
+    moment_masked_cube = moment_mask(noisy_cube, rms_noise, velocity_smoothing=4)
+
+    return moment_masked_cube, downsampled_header
+
+
+
+
+
+
 
