be06f07c1cff0afc357e66f660304df74ba552b6
Tom Rice <t.rice90@gmail.com>
2015-01-22 18:56:15 -0500

    Basic file (skeletal) for loading and transforming DHT data.

---
+++ production/load_and_process_data.py
@@ -0,0 +1,56 @@
+""" 
+This file contains the production code to load & transform datacubes.
+"""
+
+from __future__ import division
+
+import pickle
+import os.path
+import datetime
+import numpy as np
+
+import astropy
+import astrodendro
+import astropy.units as u
+import astropy.constants as c
+
+from astropy import wcs
+from astropy.io.fits import getdata, getheader
+import astropy.io.fits as fits
+
+data_path = os.path.expanduser("~/Dropbox/College/Astro99/DATA/")
+
+def load_data(filename):
+	"""
+	Loads a datacube into memory from disk using `astropy.io.fits.getdata`.
+
+	Parameters
+	----------
+	filename : str
+	    Name of the file within `data_path`.
+	    ex: "DHT17_Quad2_bw_mom.fits"
+
+	Returns
+	-------
+	datacube : np.ndarray
+	    The datacube inside `filename`. 
+	    DHT datacubes typically load in (b,l,v) order when using numpy.
+	header : astropy.io.fits.header.Header
+
+	"""
+
+	# memmap is set to True because large files can otherwise slow us down
+	datacube, header = getdata(data_path+filename, header=True, memmap=True)
+
+	return datacube, header
+
+def transpose_data_to_standard_order(datacube, header):
+
+	pass
+
+def interpolate_data():
+	pass
+
+def moment_mask_data():
+	pass
+
