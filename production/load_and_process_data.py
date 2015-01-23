0b9d7edfee9025ebbcae55244519f8eea5ddc6f4
Tom Rice <t.rice90@gmail.com>
2015-01-22 20:18:35 -0500

    Copied and modified my header transpose code for permute

--- production/load_and_process_data.py
+++ production/load_and_process_data.py
@@ -44,9 +44,57 @@ def load_data(filename):
     # memmap is set to True because large files can otherwise slow us down
     datacube, header = getdata(data_path+filename, header=True, memmap=True)
 
-def transpose_data_to_standard_order(datacube, header):
     return datacube, header
 
+def permute_data_to_standard_order(datacube, header, permute_tuple=(2,0,1)):
+    """
+    Permutes the axes of a datacube. Default case is from (b,l,v) to (v,b,l).
+
+    I chose this permutation order because of how `plt.imshow` handles 2D arrays.
+
+    Parameters
+    ----------
+    datacube : np.ndarray
+
+    header : astropy.io.fits.header.Header
+
+    permute_tuple : 3-element tuple, default: (2,0,1)
+        A tuple expressing how to permute the axes, 
+        according to np.ndarray.transpose(). 
+
+    Returns
+    -------
+    new_datacube : np.ndarray
+        The result of running `datacube.transpose(permute_tuple)`.
+    new_header : astropy.io.fits.header.Header
+        The header with the axes' properties permuted.
+
+    """
+
+    pt = permute_tuple
+
+    new_datacube = datacube.transpose(pt)
+
+    new_header = header.copy()
+
+    # let's permute. and downsample.
+    new_header['naxis'+str(pt[0]+1)] = header['naxis1']
+    new_header['naxis'+str(pt[1]+1)] = header['naxis2']
+    new_header['naxis'+str(pt[2]+1)] = header['naxis3']
+
+    new_header['ctype'+str(pt[0]+1)] = header['ctype1']
+    new_header['ctype'+str(pt[1]+1)] = header['ctype2']
+    new_header['ctype'+str(pt[2]+1)] = header['ctype3']
+
+    new_header['crval'+str(pt[0]+1)] = header['crval1']
+    new_header['crval'+str(pt[1]+1)] = header['crval2']
+    new_header['crval'+str(pt[2]+1)] = header['crval3']
+
+    new_header['cdelt'+str(pt[0]+1)] = header['cdelt1']
+    new_header['cdelt'+str(pt[1]+1)] = header['cdelt2']
+    new_header['cdelt'+str(pt[2]+1)] = header['cdelt3']
+
+    return new_datacube, new_header
 
 
 def interpolate_data():
