327e7b84bb67a55ffa3d4e44fe447b0bd31d2d45
Tom Rice <t.rice90@gmail.com>
2015-01-23 04:28:31 -0500

    i'm trying to build this into something automated (:

---
+++ production/second_quadrant_cloud_extraction.py
@@ -0,0 +1,49 @@
+""" Production code to extract a cloud catalog from the second quadrant. """
+
+import numpy as np
+
+import astropy
+import astrodendro
+import astropy.units as u
+import astropy.constants as c
+
+from .load_and_process_data import load_data, permute_data_to_standard_order
+from .compute_dendrogram_and_catalog import compute_dendrogram, compute_catalog
+from .calculate_distance_dependent_properties import assign_properties
+from .remove_degenerate_structures import reduce_catalog
+
+from ..reid_distance_assigner import make_reid_distance_column
+
+datacube, header = permute_data_to_standard_order(*load_data("DHT17_Quad2_bw_mominterp.fits"))
+d = compute_dendrogram(datacube, header, min_value=0.31, min_delta=0.31, min_npix=20)
+catalog, metadata = compute_catalog(d, header)
+
+# DISTANCE assignment
+reid_distance = make_reid_distance_column(catalog, nearfar='near')
+catalog['distance'] = reid_distance['D_k']
+
+# assignment of physical properties
+assign_properties(catalog)
+
+
+def disqualify(input_catalog):
+
+    catalog = input_catalog.copy(copy_data=True)
+
+    # narrow down how we select clouds
+    disqualified = (
+        (np.abs(catalog['v_cen']) < 25) |
+        (catalog['major_sigma'] > 10 * u.deg) |
+        (catalog['area_exact'] > 50 * u.deg**2) |
+        (catalog['mass'] < 1e4 * u.solMass) )
+
+    output_catalog = catalog[~disqualified]
+
+    return output_catalog
+
+new_catalog = disqualify(catalog)
+
+smaller_catalog = reduce_catalog(d, new_catalog)
+
+
+
