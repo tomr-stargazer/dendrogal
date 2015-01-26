2d8eeed3b41d8f7feaea321f86c70d11f7db057e
Tom Rice <t.rice90@gmail.com>
2015-01-26 03:20:32 -0500

    Moved some functionality into actual functions, for better composability

--- production/second_quadrant_cloud_extraction.py
+++ production/second_quadrant_cloud_extraction.py
@@ -14,16 +14,19 @@ from .remove_degenerate_structures import reduce_catalog
 
 from ..reid_distance_assigner import make_reid_distance_column
 
-datacube, header = permute_data_to_standard_order(*load_data("DHT17_Quad2_bw_mominterp.fits"))
-d = compute_dendrogram(datacube, header, min_value=0.31, min_delta=0.31, min_npix=20)
-catalog, metadata = compute_catalog(d, header)
+def second_quad_dendrogram():
+    datacube, header = permute_data_to_standard_order(*load_data("DHT17_Quad2_bw_mominterp.fits"))
+    d = compute_dendrogram(datacube, header, min_value=0.31, min_delta=0.31, min_npix=20)
+    catalog, metadata = compute_catalog(d, header)
 
-# DISTANCE assignment
-reid_distance = make_reid_distance_column(catalog, nearfar='near')
-catalog['distance'] = reid_distance['D_k']
+    # DISTANCE assignment
+    reid_distance = make_reid_distance_column(catalog, nearfar='near')
+    catalog['distance'] = reid_distance['D_k']
 
-# assignment of physical properties
-assign_properties(catalog)
+    # assignment of physical properties
+    assign_properties(catalog)
+
+    return d, catalog, header, metadata
 
 
 def disqualify(input_catalog):
@@ -41,9 +44,12 @@ def disqualify(input_catalog):
 
     return output_catalog
 
-new_catalog = disqualify(catalog)
+def prune_catalog(d, catalog):
+    new_catalog = disqualify(catalog)
 
-smaller_catalog = reduce_catalog(d, new_catalog)
+    smaller_catalog = reduce_catalog(d, new_catalog)
+
+    return smaller_catalog
 
 
 
