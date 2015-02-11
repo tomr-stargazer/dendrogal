8e9472c4e8a53d19a41ec4b9120a3e460194a6fc
Tom Rice <t.rice90@gmail.com>
2015-02-11 13:50:06 -0500

    Wrote a working plot_cloudmassfunction function with a fit

--- production/plot_catalog_measurements.py
+++ production/plot_catalog_measurements.py
@@ -8,7 +8,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-from .catalog_measurement import size_linewidth_slope
+from .catalog_measurement import size_linewidth_slope, cumulative_massfunction_fit, truncated_cloudmass_function
 
 
 def plot_size_linewidth_fit(catalog, title=""):
@@ -52,5 +52,36 @@ def plot_cmf_without_fit(catalog):
 	plt.ylabel("n(M > M')")
 
 	return fig
+
+def plot_cmf(catalog, bins=20, **kwargs):
+
+    fig = plt.figure()
+
+    number_in_bin_q2, bin_edges, ch = plt.hist(np.log10(catalog['mass']), cumulative=-1, log=True, bins=bins)
+    bin_centers_q2 = (bin_edges[1:] + bin_edges[:-1])/2.
+    plt.clf()
+    plt.plot(bin_centers_q2, number_in_bin_q2, 'ko' )
+    plt.semilogy()
+    plt.xlabel(r"log$_{10}$ (M$_{GMC}$ / M$_\odot$)")
+    plt.ylabel("n(M > M')")
+
+    cmf_output = cumulative_massfunction_fit(catalog, bins=bins, **kwargs)
+
+    M_0, N_0, gamma = cmf_output[0]
+
+    m_array = np.linspace(min(bin_edges), max(bin_edges), 50)
+    n_array = truncated_cloudmass_function([M_0, N_0, gamma], 10**m_array)
+
+    plt.plot(m_array, n_array, label=r"$\gamma = {0:.2f}$".format(gamma))
+
+    text_string = r"$N(M' > M) = N_0 \left [ \left ( \frac{M}{M_0} \right )^{\gamma+1} - 1 \right ]$"
+
+    plt.text(4, 1, text_string, fontsize=18)
+
+    plt.legend(loc='upper right')
+
+    return fig, cmf_output
+
+
 
 
