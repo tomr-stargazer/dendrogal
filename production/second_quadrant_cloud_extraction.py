319eea6714637d511e92b3a0bcbb32894daa6e22
Tom Rice <t.rice90@gmail.com>
2015-02-12 20:56:17 -0500

    Improved (and hopefully finalized!) the second quadrant cloud catalog.

--- production/second_quadrant_cloud_extraction.py
+++ production/second_quadrant_cloud_extraction.py
@@ -1,5 +1,7 @@
 """ Production code to extract a cloud catalog from the second quadrant. """
 
+from __future__ import division
+
 import numpy as np
 
 import astropy
@@ -13,10 +15,11 @@ from .calculate_distance_dependent_properties import assign_properties
 from .remove_degenerate_structures import reduce_catalog
 
 from ..reid_distance_assigner import make_reid_distance_column
+from ..catalog_tree_stats import compute_tree_stats
 
 def second_quad_dendrogram():
     datacube, header = permute_data_to_standard_order(*load_data("DHT17_Quad2_bw_mominterp.fits"))
-    d = compute_dendrogram(datacube, header, min_value=0.31, min_delta=0.31, min_npix=20)
+    d = compute_dendrogram(datacube, header, min_value=0.31/2, min_delta=0.31, min_npix=20)
     catalog, metadata = compute_catalog(d, header)
 
     # DISTANCE assignment
@@ -25,6 +28,9 @@ def second_quad_dendrogram():
 
     # assignment of physical properties
     assign_properties(catalog)
+
+    # assignment of tree statistic properties
+    compute_tree_stats(catalog, d)    
 
     return d, catalog, header, metadata
 
@@ -36,8 +42,12 @@ def disqualify(input_catalog):
     # narrow down how we select clouds
     disqualified = (
         (np.abs(catalog['v_cen']) < 25) |
-        (catalog['major_sigma'] > 2 * u.deg) |
-        (catalog['area_exact'] > 50 * u.deg**2) |
+        # (catalog['major_sigma'] > 2 * u.deg) |
+        # (catalog['area_exact'] > 50 * u.deg**2) |
+        (catalog['fractional_gain'] > 0.81) | 
+        (catalog['n_descendants'] > 15) |
+        (catalog['x_sol'] < -7.5) | # screens out kdist errors near l=180
+        (catalog['v_cen'] < -100 ) | # no parts of the galaxy are going this fast
         (catalog['mass'] < 10**3.5 * u.solMass) )
 
     output_catalog = catalog[~disqualified]
