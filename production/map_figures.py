1c2522a69ad7fdd96188f2543e1d4f9a21fc3ef3
Tom Rice <t.rice90@gmail.com>
2015-03-06 03:57:44 +0000

    I'm working on a map figures code!
    It makes map figures.

---
+++ production/map_figures.py
@@ -0,0 +1,114 @@
+""" This is a script that will help us create figure maps. """
+
+from __future__ import division
+import os.path
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
+import astropy
+import astrodendro
+import astropy.units as u
+import astropy.constants as c
+
+from ..integrated_viewer import IntegratedViewer
+from .remove_degenerate_structures import reduce_catalog, selection_from_catalog
+from ..hurt_image_overlay_demo import underlay_hurt_galaxy
+
+# basic function: take in something analogous to an IntegratedViewer, and then let's figure out how to put like ellipses on it.
+
+# this will require some WCS stuff  (WCSAxes almost certainly).
+
+output_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/paper/")
+
+def make_quadrant_lbv_map(cloud_catalog, dendgrogram, contour_select=True, alignment='lv'):
+
+    # one: select things
+
+    d = dendgrogram
+    dv = d.viewer()
+
+    cloud_selection = selection_from_catalog(d, cloud_catalog)
+
+    if contour_select:
+        dv.hub.select(1, cloud_selection, subtree=False)
+        dv.hub.select(2, None)
+        dv.hub.select(3, None)
+
+    iv = IntegratedViewer(d, dv.hub, cmap='gray_r', clip_velocity=False, 
+                          alignment=alignment, aspect=1/3)
+
+    # let's make ellipses
+    # copied largely from comparison_to_other_catalogs.py
+
+    world_coordinates = np.vstack([cloud_catalog['x_cen'], cloud_catalog['y_cen'], cloud_catalog['v_cen']]).T
+
+    lbv_pixels = iv.dendrogram.wcs.wcs_world2pix(world_coordinates, 0)
+
+    l_lbv_pixels = lbv_pixels[:,0]
+    b_lbv_pixels = lbv_pixels[:,1]
+    v_lbv_pixels = lbv_pixels[:,2]
+
+    # l_scale_lv = (l_lv_pixels[1] - l_lv_pixels[0])/(lcen_column[1]-lcen_column[0])
+    # v_scale_lv = (v_lv_pixels[1] - v_lv_pixels[0])/(vcen_column[1]-vcen_column[0])
+    l_scale_lbv = 0.125
+    b_scale_lbv = 0.125
+    v_scale_lbv = 0.65
+
+    lv_ells = [Ellipse(xy=zip(l_lbv_pixels, v_lbv_pixels)[i], 
+                       width=2*cloud_catalog['major_sigma'][i]/l_scale_lbv, 
+                       height=2*cloud_catalog['v_rms'][i]/v_scale_lbv) for i in range(len(cloud_catalog))]
+    for e in lv_ells:
+        iv.ax_lv.add_artist(e)
+        e.set_facecolor('pink')
+        e.set_edgecolor('pink')
+        e.set_edgecolor('black')
+        e.set_linewidth(0.5)
+        e.set_zorder(1)
+
+    iv.ax_lv.set_xlabel(r"Galactic longitude $l$ (deg)")
+    iv.ax_lv.set_ylabel(r"Velocity $v_{LSR}$ (km s$^{-1}$)")            
+
+    if alignment != 'lv':
+        lb_ells = [(Ellipse(xy=zip(l_lbv_pixels, b_lbv_pixels)[i], 
+                           angle=cloud_catalog['position_angle'][i],
+                           width=2*cloud_catalog['major_sigma'][i]/l_scale_lbv, 
+                           height=2*cloud_catalog['minor_sigma'][i]/b_scale_lbv),
+                    cloud_catalog['distance'][i]) for i in range(len(cloud_catalog))]
+
+        for e, distance in lb_ells:
+            iv.ax_lb.add_artist(e)
+            e.set_facecolor('pink')
+            e.set_edgecolor('black')
+            e.set_linewidth(0.5)
+            e.set_zorder(2 - 0.001 * distance)
+
+        iv.ax_lb.set_xlabel(r"Galactic longitude $l$ (deg)")
+        iv.ax_lb.set_ylabel(r"Galactic latitude $b$ (deg)")
+
+    iv.fig.canvas.draw()
+
+    return iv
+
+def make_quadrant_topdown_map(cloud_catalog, loc=None):
+
+    fig = plt.figure()
+
+    smalls = cloud_catalog[cloud_catalog['mass'] < 1e5]
+    mediums = cloud_catalog[(cloud_catalog['mass'] < 1e6) & (cloud_catalog['mass'] > 1e5)]
+    bigs = cloud_catalog[(cloud_catalog['mass'] < 1e7) & (cloud_catalog['mass'] > 1e6)]
+    giants = cloud_catalog[cloud_catalog['mass'] > 1e7]
+
+    plt.plot(smalls['y_sol'], smalls['x_sol'], 'r.', label=r"$M \sim 10^4 M_\odot$")
+    plt.plot(mediums['y_sol'], mediums['x_sol'], 'bo', label=r"$M \geq 10^5 M_\odot$")
+    plt.plot(bigs['y_sol'], bigs['x_sol'], 'y*', ms=15, label=r"$M \geq 10^6 M_\odot$")
+    plt.plot(giants['y_sol'], giants['x_sol'], 'gs', label=r"$M \geq 10^7 M_\odot$")
+
+    leg = plt.legend(loc=loc, scatterpoints=1)
+    leg.get_frame().set_alpha(0.5)
+
+    underlay_hurt_galaxy(fig, u.kpc)
+
+    return fig
+
