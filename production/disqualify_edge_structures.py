3bb28afe7c573300aa5cd7fb9c9c1451e0a1f374
Tom Rice <t.rice90@gmail.com>
2015-03-17 04:45:50 -0400

    fixed an ordering issue, and refactored.

--- production/disqualify_edge_structures.py
+++ production/disqualify_edge_structures.py
@@ -7,24 +7,36 @@ from __future__ import division
 
 import numpy as np
 
+def on_edge(struct, shape):
+    """ Checks whether a given struct is on an edge. """
+
+    x_len, y_len, z_len = shape
+    x_array, y_array, z_array = struct.indices()
+
+    if ((0 in x_array) or
+        (0 in y_array) or
+        (0 in z_array) or
+        (x_len-1 in x_array) or
+        (y_len-1 in y_array) or
+        (z_len-1 in z_array)
+        ):
+        return True
+    else:
+        return False
+
 def identify_edge_structures(d):
+    """ 
+    Creates an array identifying which structures are on an edge. 
 
-    x_len, y_len, z_len = d.data.shape
+    Sorted by _idx, not by height - so it follows a catalog order, 
+    not a dendrogram order.
 
-    on_edge = np.zeros(len(d), dtype=int)
+    """
 
-    for i, struct in enumerate(d):
+    shape = d.data.shape
+    on_edge_array = np.zeros(len(d), dtype=int)
 
-        x_array, y_array, z_array = struct.indices()
-        if ((0 in x_array) or
-            (0 in y_array) or
-            (0 in z_array) or
-            (x_len-1 in x_array) or
-            (y_len-1 in y_array) or
-            (z_len-1 in z_array)
-            ):
-            on_edge[i] = 1
-        else:
-            pass
+    for i in range(len(d)):
+        on_edge_array[i] = int(on_edge(d[i], shape))
 
-    return on_edge
+    return on_edge_array
