620861a86a1ea7d019a350c3b81598a8cdbe2d68
Tom Rice <t.rice90@gmail.com>
2015-03-17 02:27:44 -0400

    added the Garcia catalog to comparisons. This works.

--- comparison_to_other_catalogs.py
+++ comparison_to_other_catalogs.py
@@ -22,11 +22,14 @@ from astropy import table
 data_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/other_catalogs/")
 
 dame86_catalog = table.Table.read(data_path+'Dame+86_catalog_sanitized.txt', format='ascii', header_start=2)
+garcia_catalog = table.Table.read(data_path+'Garcia_catalog.fit')
 
 id_column = dame86_catalog['ID']
 vcen_column = np.array([x.split(',')[1] for x in id_column]).astype('float')
 lcen_column = dame86_catalog['LII']
 sky_radius_column = np.degrees(dame86_catalog['R']/(dame86_catalog['D']*1000))
+
+garcia_sky_radius = np.degrees(garcia_catalog['R']/(garcia_catalog['D']*1000))
 
 def make_figures_to_show_lv_stuff():
     fig1 = plt.figure()
@@ -74,4 +77,34 @@ def plot_dame_ellipses_on_integrated_viewer(integrated_viewer):
         iv.ax_lv.add_artist(e)
         e.set_facecolor('none')
         e.set_edgecolor('blue')
+
+
+def plot_garcia_ellipses_on_iv(integrated_viewer):
+
+    iv = integrated_viewer
+
+    world_coordinates = np.vstack([garcia_catalog['GLON'].data, garcia_catalog['GLAT'].data, garcia_catalog['Vlsr'].data]).T
+
+    lv_pixels = iv.dendrogram.wcs.wcs_world2pix(world_coordinates, 0)
+
+    lbv_pixels = iv.dendrogram.wcs.wcs_world2pix(world_coordinates, 0)
+
+    l_lbv_pixels = lbv_pixels[:,0]
+    b_lbv_pixels = lbv_pixels[:,1]
+    v_lbv_pixels = lbv_pixels[:,2]
+
+    l_scale_lbv = 0.125
+    b_scale_lbv = 0.125
+    v_scale_lbv = 1.3 
+
+    lv_ells = [Ellipse(xy=zip(l_lbv_pixels, v_lbv_pixels)[i], 
+                       width=2*garcia_sky_radius[i]/l_scale_lbv, 
+                       height=garcia_catalog['DV'][i]/v_scale_lbv) for i in range(len(garcia_catalog))]
+    for e in lv_ells:
+        iv.ax_lv.add_artist(e)
+        e.set_facecolor('none')
+        e.set_edgecolor('blue')
+        e.set_zorder(2)
+
+    iv.fig.canvas.draw()
 
