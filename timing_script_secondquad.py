0e85ebd8197de8872db96a5ba528c62124467861
Tom Rice <t.rice90@gmail.com>
2015-03-19 19:42:27 -0400

    wrote a timing script - it was really useful for profiling!

---
+++ timing_script_secondquad.py
@@ -0,0 +1,17 @@
+"""
+To be called like this:
+
+python -m cProfile timing_script_secondquad.py
+
+"""
+
+print "Good morning."
+
+from astrodendro_analysis.production.second_quadrant_cloud_extraction import second_quad_dendrogram
+
+print "I have imported the second_quad_dendrogram function."
+print "Now I am going to call it:"
+
+d2, catalog2, header2, metadata2 = second_quad_dendrogram()
+
+print "I have finished."
