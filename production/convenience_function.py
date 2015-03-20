c8f7cb3390d0252828e882414b5656f8b3317351
Tom Rice <t.rice90@gmail.com>
2015-03-19 20:34:30 -0400

    I wrote the first part of the save/reload memoizing, and a test for it.

--- production/convenience_function.py
+++ production/convenience_function.py
@@ -1,4 +1,18 @@
-""" Saves us some time. """
+""" 
+Combines loading and computing dendrograms into one function.
+
+I am implementing caching options to save even more time. 
+
+"""
+
+import os.path
+from functools import wraps
+import pickle
+# import astropy.io.fits as fits
+import astropy
+from astropy.io.fits import getheader
+
+import astrodendro
 
 from .load_and_process_data import load_data, permute_data_to_standard_order
 from .compute_dendrogram_and_catalog import compute_dendrogram, compute_catalog
@@ -11,3 +25,47 @@ def load_permute_dendro_catalog(filename, min_value=None, min_delta=None, min_np
     catalog, metadata = compute_catalog(d, header)
 
     return d, catalog, header, metadata 
+
+savepath = os.path.expanduser("~/Documents/Code/astrodendro_analysis/production/saved_dendrograms/")
+
+def save_dendrogram_catalog_output(d, catalog, header, metadata, **kwargs):
+
+    filename_dict = filename_generator(**kwargs)
+
+    d.save_to(filename_dict['d'])
+    catalog.write(filename_dict['catalog'], overwrite=True)
+    header.tofile(filename_dict['header'], clobber=True)
+    pickle.dump(metadata, open(filename_dict['metadata'], 'wb'))
+
+    return None
+
+
+def filename_generator(data_filename=None, min_value=None, min_delta=None, min_npix=None, savepath=savepath):
+
+    filename_dict = {}
+
+    if data_filename is None: 
+        raise ValueError("`data_filename` must be provided!")
+
+    if min_value is None:
+        min_value_str = str(min_value)
+    else:
+        min_value_str = '{:.3f}'.format(min_value)
+
+    if min_delta is None:
+        min_delta_str = str(min_delta)
+    else:
+        min_delta_str = '{:.3f}'.format(min_delta)
+
+    min_npix_str = str(min_npix)
+
+    filename_base = "{0}_{1}_{2}_{3}".format(data_filename, min_value_str, min_delta_str, min_npix_str)
+
+    filename_dict['d'] = savepath+filename_base+"_d.hdf5"
+    filename_dict['catalog'] = savepath+filename_base+"_catalog.fits"
+    filename_dict['header'] = savepath+filename_base+"_header.fits"
+    filename_dict['metadata'] = savepath+filename_base+"_metadata.p"
+
+    return filename_dict
+
+
---
+++ production/test/test_convenience_function.py
@@ -0,0 +1,81 @@
+"""
+Tests for convenience_function.py
+
+should run with py.test
+
+"""
+
+import os
+import pickle
+
+from numpy.testing import assert_allclose, assert_equal, assert_array_equal
+import numpy as np
+
+from astropy.table import Table
+from astropy.io.fits import getdata, getheader, Header, writeto
+import astropy.units as u
+from astrodendro import Dendrogram
+
+from ..convenience_function import save_dendrogram_catalog_output, reload_dendrogram_catalog_output
+
+filepath = "production/test/"
+
+def test_filename_generator():
+    pass
+
+def test_save_dendrogram_catalog_output():
+
+    # 1. construct a silly dendrogram, catalog, header, and metadata
+    data = np.zeros((3,3,3))
+    data[1,1,1] = 1
+    d = Dendrogram.compute(data, min_value=0)
+
+    catalog = Table()
+    catalog['test_column'] = np.zeros(10)
+
+    header = Header.fromstring("SIMPLE  =                    T / conforms to FITS standard                      BITPIX  =                   64 / array data type                                NAXIS   =                    3 / number of array dimensions                     NAXIS1  =                    6                                                  NAXIS2  =                    5                                                  NAXIS3  =                    4                                                  CTYPE1  = 'VELO-LSR'                                                            CTYPE2  = 'GLON-CAR'                                                            CTYPE3  = 'GLAT-CAR'                                                            CRVAL1  = ''                                                                    CRVAL2  = ''                                                                    CRVAL3  = ''                                                                    CDELT1  = ''                                                                    CDELT2  = ''                                                                    CDELT3  = ''                                                                    CRPIX1  = ''                                                                    CRPIX2  = ''                                                                    CRPIX3  = ''                                                                    END                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ")
+
+    metadata = {}
+    metadata['data_unit'] = u.K
+
+    # 2. run save_dendrogram_catalog_output on them - to some safe location
+    kwargs = {'data_filename': 'not_real_data.fits', 
+              'min_value': 1, 
+              'min_delta': 2, 
+              'min_npix': 3,
+              'savepath': filepath}
+    save_dendrogram_catalog_output(d, catalog, header, metadata, **kwargs)
+
+    # 3. reload those things 
+    filename_base = filepath+"not_real_data.fits_1.000_2.000_3"
+
+    d2 = Dendrogram.load_from(filename_base+"_d.hdf5")
+    catalog2 = Table.read(filename_base+"_catalog.fits")
+    header2 = getheader(filename_base+"_header.fits")
+    metadata2 = pickle.load(open(filename_base+"_metadata.p", 'rb'))
+
+    #4 and assert they equal the mock things.
+    assert_equal(d2.index_map, d.index_map)
+    assert_array_equal(catalog2, catalog)
+    assert_equal(header2, header)
+    assert_equal(metadata2, metadata)
+
+    #5 then delete the saved objects.
+    os.remove(filename_base+"_d.hdf5")
+    os.remove(filename_base+"_catalog.fits")
+    os.remove(filename_base+"_header.fits")
+    os.remove(filename_base+"_metadata.p")
+
+def test_reload_dendrogram_catalog_output():
+    pass
+
+# def reload_dendrogram_catalog_output(**kwargs):
+
+#     filename_dict = filename_generator(**kwargs)
+
+#     d = astrodendro.Dendrogram.load_from(filename_dict['d'])
+#     catalog = astropy.table.Table.read(filename_dict['catalog'])
+#     header = getheader(filename_dict['header'])
+#     metadata = pickle.load(open(filename_dict['metadata'], 'rb'))
+
+#     return d, catalog, header, metadata
