ab38940efd79e4058c148b9f4eb84fe38282922c
Tom Rice <t.rice90@gmail.com>
2015-03-29 12:41:16 -0400

    starting a new plotting class.

---
+++ production/integrated_map_figure.py
@@ -0,0 +1,10 @@
+"""
+A class that makes Integrated Map Figures. 
+
+Inspired largely by IntegratedViewer, but 
+(a) with the interactive/synchronization with a Viewer.hub bits stripped out
+(b) more fine control over integration ranges
+(c) better use of WCS information.
+
+"""
+
