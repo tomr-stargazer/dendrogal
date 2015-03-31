51bd17e64badd25ffddf3873cde1aada32892e77
Tom Rice <t.rice90@gmail.com>
2015-03-31 00:33:49 -0400

    improved the generate grid script.

--- production/generate_firstquad_extraction_grid.py
+++ production/generate_firstquad_extraction_grid.py
@@ -8,44 +8,59 @@ from __future__ import division
 import numpy as np
 
 from astrodendro_analysis.production import first_quadrant_cloud_extraction, map_figures
+from astrodendro_analysis.comparison_to_other_catalogs import plot_dame_ellipses_on_imf
 
-max_tree_list = [10, 20, 30]
-min_val_list = [0.18/2, 0.18]
-min_delta_list = [0.18/2, 0.18]
-min_tree_list = [1, 0]
+colorbrewer_red = '#e41a1c' 
+colorbrewer_blue = '#377eb8'
+colorbrewer_green = '#4daf4a'
 
-print "Total upcoming iterations: {0}".format(len(max_tree_list)*len(min_val_list)*len(min_delta_list)*len(min_tree_list))
+min_npix_list = [20, 30, 40]
+max_tree_list = [10, 30]
+min_val_list = [0.18]
+min_delta_list = [0.18]
+min_tree_list = [2, 0]
+
+print "Total upcoming iterations: {0}".format(len(max_tree_list)*len(min_val_list)*len(min_delta_list)*len(min_tree_list)*len(min_npix_list))
 
 directory = 'grid_of_cloud_parameters/'
 
 def run_metrics_on(cloud_catalog, dendrogram, 
                    min_val=None, min_delta=None, 
-                   max_tree=None, min_tree=None):
+                   max_tree=None, min_tree=None, min_npix=None):
 
     # make and save some plots
 
-    fig = map_figures.make_lv_map_new(cloudy1, d1, ellipse_color=colorbrewer_blue)
-    min_value_str = '{:.3f}'.format(min_value)
+    imf = map_figures.make_lv_map_new(cloud_catalog, dendrogram, ellipse_color=colorbrewer_blue)
+    min_value_str = '{:.3f}'.format(min_val)
     min_delta_str = '{:.3f}'.format(min_delta)
     max_tree_str = str(max_tree)
     min_tree_str = str(min_tree)
+    min_npix_str = str(min_npix)
     
-    filename = "{0}_{1}_{2}_{3}.png".format(min_value_str, min_delta_str, max_tree_str, min_tree_str)
+    filename = "np{4}_mv{0}_md{1}_xt{2}_nt{3}.png".format(min_value_str, min_delta_str, max_tree_str, min_tree_str, min_npix_str)
 
-    fig.savefig(directory+filename, bbox_inches='tight')
+    imf.fig.savefig(directory+filename, bbox_inches='tight')
+
+    plot_dame_ellipses_on_imf(imf)
+
+    imf.fig.savefig(directory+'dame_'+filename, bbox_inches='tight')
+
+
 
 for min_val in min_val_list:
 
     for min_delta in min_delta_list:
 
-        args = first_quadrant_cloud_extraction.first_quad_dendrogram(min_value=min_val, min_delta=min_delta)
-        d, catalog, header, metadata = args
+        for min_npix in min_npix_list:
 
-        for max_tree in max_tree_list:
+            args = first_quadrant_cloud_extraction.first_quad_dendrogram(min_value=min_val, min_delta=min_delta, min_npix=min_npix)
+            d, catalog, header, metadata = args
 
-            for min_tree in min_tree_list:
+            for max_tree in max_tree_list:
 
-                cloud_catalog = export_firstquad_catalog(args, max_tree=max_tree, min_tree=min_tree)
+                for min_tree in min_tree_list:
 
-                run_metrics_on(cloud_catalog, d, min_val, min_delta, max_tree, min_tree)
+                    cloud_catalog = first_quadrant_cloud_extraction.export_firstquad_catalog(args, max_descendants=max_tree, min_descendants=min_tree)
+
+                    run_metrics_on(cloud_catalog, d, min_val, min_delta, max_tree, min_tree, min_npix)
 
