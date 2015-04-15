213999bfd57267debda0200cc0597a451822da3b
Tom Rice <t.rice90@gmail.com>
2015-04-14 23:29:37 -0400

    Here's where the Carina data is loaded.

---
+++ production/make_carina_stub.py
@@ -0,0 +1,24 @@
+""" 
+Loads and computes the Carina dendrogram, so that it can be imported. 
+
+Since the dendrogram parameters are (at some fundamental level) now fixed, 
+having this stub allows us to run & vary the cloud extraction many times
+without having to reload the dendrogram & catalog itself, allowing us to
+avoid a time-bottleneck.
+
+"""
+
+from __future__ import division
+
+from astrodendro_analysis.production.convenience_function import load_permute_dendro_catalog
+
+min_value = 0.17/2
+min_delta = 0.17
+min_npix = 20
+
+data_filename = "DHT33_Carina_mominterp.fits"
+dendrogram_kwargs = {'min_value': min_value,
+                     'min_delta': min_delta,
+                     'min_npix': min_npix}
+
+d, catalog, header, metadata = load_permute_dendro_catalog(data_filename, **dendrogram_kwargs)
