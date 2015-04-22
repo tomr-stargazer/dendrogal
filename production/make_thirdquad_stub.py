8dae84f1a6939c61b01c93077e7b2a39ca46563e
Tom Rice <t.rice90@gmail.com>
2015-04-22 13:28:36 -0400

    materials for q3 cloud extraction

---
+++ production/make_thirdquad_stub.py
@@ -0,0 +1,20 @@
+""" 
+Loads and computes the third quadrant dendrogram, so that it can be imported. 
+
+Since the dendrogram parameters are (at some fundamental level) now fixed, 
+having this stub allows us to run & vary the cloud extraction many times
+without having to reload the dendrogram & catalog itself, allowing us to
+avoid a time-bottleneck.
+
+"""
+
+from __future__ import division
+
+from astrodendro_analysis.production.convenience_function import load_permute_dendro_catalog
+
+data_filename = "DHT31_Quad3_mominterp.fits"
+dendrogram_kwargs = {'min_value' : 0.12/2,
+                     'min_delta' : 0.12,
+                     'min_npix' : 20}
+
+d, catalog, header, metadata = load_permute_dendro_catalog(data_filename, **dendrogram_kwargs)
---
+++ production/new_cloud_extractor_q3.py
@@ -0,0 +1,93 @@
+"""
+Extracts clouds from the third quadrant using the new cloud-first method.
+
+"""
+
+from __future__ import division
+
+import numpy as np
+
+import astropy
+
+from astrodendro_analysis.production.calculate_distance_dependent_properties import assign_properties
+from astrodendro_analysis.production.remove_degenerate_structures import reduce_catalog
+from astrodendro_analysis.production.disqualify_edge_structures import identify_edge_structures
+from astrodendro_analysis.production.convenience_function import load_permute_dendro_catalog
+
+from astrodendro_analysis.reid_distance_assigner import make_reid_distance_column
+from astrodendro_analysis.catalog_tree_stats import compute_tree_stats
+
+from make_thirdquad_stub import d, catalog, header, metadata
+
+def third_quad_cloud_catalog():
+    """
+    This generates a 'processed' dendrogram catalog, made
+    from the global, pre-loaded `catalog` from the third quadrant survey.
+
+    It identifies edge structures, computes tree statistics,
+    determines the kinematic distances, and then assigns physical
+    properties to the structures.
+
+    Its output is meant to be the input to `get_positive_velocity_clouds()`,
+    and `get_negative_velocity_clouds()`.
+
+    """
+
+    catalog_cp = catalog.copy(copy_data=True)
+
+    # assignment of tree statistic properties
+    compute_tree_stats(catalog_cp, d)
+
+    # note edge structures
+    catalog_cp['on_edge'] = identify_edge_structures(d)
+
+    # DISTANCE assignment
+    distance_table = make_reid_distance_column(catalog, nearfar='near')
+    catalog_cp['distance'] = distance_table['D_k']
+
+    # assignment of physical properties to unambigously-distanced structures
+    assign_properties(catalog_cp)
+
+    return catalog_cp
+
+
+def get_positive_velocity_clouds(input_catalog, max_descendants=15):
+
+    catalog = input_catalog.copy(copy_data=True)
+
+    # narrow down how we select clouds
+    disqualified_location = (
+        ((catalog['v_cen'] < 25) & (catalog['x_cen'] > 205)) |
+        ((catalog['v_cen'] < 12) & (catalog['x_cen'] < 205)) |
+        (catalog['x_cen'] > 280) |
+        (catalog['on_edge'] == 1) |
+        (catalog['x_sol'] < -7) 
+    )
+
+    pre_output_catalog = catalog[~disqualified_location]
+
+    # now it's just got clouds that COULD be real
+    almost_output_catalog = reduce_catalog(d, pre_output_catalog)
+
+    # these objects already have mass, size etc computed so that's fine
+    final_qualified = (almost_output_catalog['mass'] > 3e3)
+    output_catalog = almost_output_catalog[final_qualified]
+
+    return output_catalog
+
+# this function is totally redundant, but is kept for consistency with the other things.
+def compile_thirdquad_catalog(input_catalog):
+
+    positive_v_catalog = get_positive_velocity_clouds(input_catalog)
+
+    composite_unreduced_catalog = astropy.table.vstack([positive_v_catalog])
+
+    composite_reduced_catalog = reduce_catalog(d, composite_unreduced_catalog)
+
+    return composite_reduced_catalog
+
+
+def export_thirdquad_catalog():
+
+    return compile_thirdquad_catalog(third_quad_cloud_catalog())
+
