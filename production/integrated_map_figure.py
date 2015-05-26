2af8cebfcb27abd06e295ec0729e9c1c69c83437
Tom Rice <t.rice90@gmail.com>
2015-05-26 13:06:47 -0400

    here are the integrated_map_figure changes needed for the thumbnails to work!
    I added a couple of functions, but didn't change functionality on previous code.

--- production/integrated_map_figure.py
+++ production/integrated_map_figure.py
@@ -30,6 +30,18 @@ def latitude_world2pix(wcs, latitudes):
 
     return pixels[:,1]
 
+
+def velocity_world2pix(wcs, velocities):
+    # assumes an l, b, v datacube (or at least that axis=2 is the velocity axis)
+
+    filler = np.repeat(0, len(velocities))
+
+    world_coordinates = np.vstack([filler, filler, velocities]).T
+    pixels = wcs.wcs_world2pix(world_coordinates, 0)
+
+    return pixels[:,2]
+
+
 class IntegratedMapFigure(object):
 
     def __init__(self, datacube, wcs_object,  
@@ -77,3 +89,59 @@ class IntegratedMapFigure(object):
         self.ax.contour(mask_lv, **kwargs)
 
         self.fig.canvas.draw()
+
+
+def integrated_map_axes_lb(fig, ax_limits, datacube, wcs_object, integration_limits,
+                           cmap='RdGy_r' ):
+    """ 
+    Hackish way of exporting the above behavior to an ax object.
+
+    But it's actually on-the-sky.
+
+    """
+
+    ax = WCSAxes(fig, ax_limits, wcs=wcs_object, 
+                     slices=('x', 'y', 0))
+    fig.add_axes(ax)
+
+    min_v_px, max_v_px = velocity_world2pix(wcs_object, integration_limits)
+
+    array_lb = np.nansum(datacube[min_v_px:max_v_px,:,:], axis=0)
+    array_lb[(array_lb < 0) | np.isinf(array_lb) | np.isnan(array_lb)] = 0
+
+    ax.spatial_scale = np.abs(wcs_object.wcs.cdelt[0]) * u.deg
+    ax.velocity_scale = np.abs(wcs_object.wcs.cdelt[2]) * u.km/u.s    
+
+    image_lb = ax.imshow(np.log10(array_lb+1), origin='lower', 
+        interpolation='nearest', cmap=cmap, aspect=1)
+
+    return ax
+
+
+def integrated_map_axes_lv(fig, ax_limits, datacube, wcs_object, integration_limits,
+                           aspect_in_units=5*(u.km/u.s)/u.deg,
+                           cmap='RdGy_r'):
+    """ 
+    Hackish way of exporting the above behavior to an ax object.
+
+    """
+
+    ax = WCSAxes(fig, ax_limits, wcs=wcs_object, 
+                     slices=('x', 0, 'y'))
+    fig.add_axes(ax)
+
+    min_b_px, max_b_px = latitude_world2pix(wcs_object, integration_limits)
+
+    array_lv = np.nansum(datacube[:,min_b_px:max_b_px,:], axis=1)
+    array_lv[(array_lv < 0) | np.isinf(array_lv) | np.isnan(array_lv)] = 0
+
+    ax.spatial_scale = np.abs(wcs_object.wcs.cdelt[0]) * u.deg
+    ax.velocity_scale = np.abs(wcs_object.wcs.cdelt[2]) * u.km/u.s
+
+    ax.aspect_px = (ax.velocity_scale/ax.spatial_scale)/aspect_in_units
+
+    image_lv = ax.imshow(np.log10(array_lv+1), origin='lower', 
+        interpolation='nearest', cmap=cmap, aspect=ax.aspect_px)
+
+    return ax    
+
