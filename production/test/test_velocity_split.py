9491fb468246b05cea8f994b0227b081eae71dcf
Tom Rice <t.rice90@gmail.com>
2015-05-27 14:17:30 -0400

    revamped this test case to allow descendant_max_vsplit to be tested.

--- production/test/test_velocity_split.py
+++ production/test/test_velocity_split.py
@@ -10,27 +10,39 @@ from numpy.testing import assert_allclose, assert_equal
 
 import astropy.table
 
-from ..velocity_split import calculate_velocity_split
+from ..velocity_split import calculate_velocity_split, descendants_max_vsplit
 
 def test_calculate_velocity_split():
 
-    mock_dendrogram = {}
+    class MockDendrogram():
+        def __init__(self):
+            self.dict = {}
+
+        def __getitem__(self, key):
+            return self.dict[key]
+
+        def __iter__(self):
+            return iter(self.dict.values())
+
+    mock_dendrogram = MockDendrogram()
 
     class MockStruct():
         def __init__(self, idx, children=[]):
             self.idx = idx
             self.children = children
-            mock_dendrogram[idx] = self
+            self.descendants = children # this is meant to be manually overridden in the one necessary case
+            mock_dendrogram.dict[idx] = self
 
-    test_struct1 = MockStruct(1)
-    test_struct2 = MockStruct(2)
-    test_struct3 = MockStruct(3, children=[test_struct1, test_struct2])
-    test_struct4 = MockStruct(4)
-    test_struct5 = MockStruct(5, children=[test_struct3, test_struct4])
+    test_struct1 = MockStruct(0)
+    test_struct2 = MockStruct(1)
+    test_struct3 = MockStruct(2, children=[test_struct1, test_struct2])
+    test_struct4 = MockStruct(3)
+    test_struct5 = MockStruct(4, children=[test_struct3, test_struct4])
+    test_struct5.descendants = [test_struct1, test_struct2, test_struct3, test_struct4]
 
     test_catalog = astropy.table.Table()
     
-    test_idx_column = [1, 2, 3, 4, 5]
+    test_idx_column = [0, 1, 2, 3, 4]
     test_vcen_column = [-5, 5, -1, 5, 3]
     test_catalog['_idx'] = test_idx_column
     test_catalog['v_cen'] = test_vcen_column
@@ -41,6 +53,10 @@ def test_calculate_velocity_split():
 
     assert_equal(velocity_split, expected_split)
 
+    test_catalog['v_split'] = velocity_split
+    test_catalog['n_descendants'] = [0, 0, 2, 0, 4]
+    max_vsplit = descendants_max_vsplit(mock_dendrogram, test_catalog)
 
-
+    excpected_max_split = [0, 0, 4, 0, 4]
+    assert_equal(max_vsplit, excpected_max_split)
 
