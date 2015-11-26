8d7fff372fb8e4558527180343bc8ba2b7a72be8
Tom Rice <t.rice90@gmail.com>
2015-11-25 23:24:05 -0500

    currently the send_mspec_command_to_idl function FINALLY works

--- production/mspecfit_wrapper.py
+++ production/mspecfit_wrapper.py
@@ -3,53 +3,114 @@ Takes a given catalog and returns the mass spectrum fits.
 
 """
 
-def multipanel_write_masses_and_errors_to_file_output():
+from __future__ import division
+import os
+import subprocess
+
+import numpy as np
+
+from dendrogal.production.config import idl_code_path
+
+
+def prepare_stuff_for_mspecfit(catalog, name):
     """
     Makes an output suitable for IDL mspecfit stuff.
 
     """
 
-    sky_text_dict = {'north': 'Northern', 'south': 'Southern', 'north_plus_south' : 'All'}
-    gal_text_dict = {'inner': ' inner Galaxy', 'outer': ' outer Galaxy', 'inner_plus_outer' : ' Galaxy (combined)'}
+    mass = catalog['mass']
+    mass_err = np.sqrt(catalog['error_mass_plus']*catalog['error_mass_minus'])
 
-    sky_subset_dict = {}
-    sky_subset_dict['north'] = pruned_catalog['x_cen'] < 180
-    sky_subset_dict['south'] = pruned_catalog['x_cen'] > 180
-    sky_subset_dict['north_plus_south'] = pruned_catalog['x_cen'] != 0
+    fname_mass = os.path.join(idl_code_path, name+'mass.txt')
+    fname_err = os.path.join(idl_code_path, name+'err.txt')
 
-    gal_subset_dict = {}
-    gal_subset_dict['inner'] = pruned_catalog['R_gal'] < 8
-    gal_subset_dict['outer'] = pruned_catalog['R_gal'] > 9
-    gal_subset_dict['inner_plus_outer'] = pruned_catalog['R_gal'] != 0
+    np.savetxt(fname_mass, mass)
+    np.savetxt(fname_err, mass_err)
 
-    # the core loop.
-    for i, sky_region in enumerate(['north', 'south', 'north_plus_south']):
 
-        for j, galactic_region in enumerate(['inner', 'outer', 'inner_plus_outer']):
+def send_mspec_command_to_idl(name, notrunc=1):
 
-            if (galactic_region == 'inner_plus_outer') and (sky_region != 'north_plus_south'):
-                continue
+    cwd = os.getcwd()
 
-            if galactic_region == 'outer':
-                max_mass = 3e7
-                min_mass = 1e4
-            else:
-                max_mass = 3e7
-                min_mass = 1e5
+    idl_command = "/Applications/exelis/idl83/bin/idl"
 
-            catalog = pruned_catalog[sky_subset_dict[sky_region] & gal_subset_dict[galactic_region] & 
-                                    (~np.isnan(pruned_catalog['mass'])) &
-                                    (pruned_catalog['mass']>min_mass) & (pruned_catalog['mass']<max_mass)]
 
-            mass = catalog['mass']
-            mass_err = np.sqrt(catalog['error_mass_plus']*catalog['error_mass_minus'])
+    # os.chdir(idl_code_path)
 
-            path = os.path.expanduser("~/Documents/Code/idl-low-sky/eroslib/")
+    p1 = subprocess.Popen(["echo", 'read_files_noise_experiment(\'{0}\', {1})'.format(name, notrunc)], stdout=subprocess.PIPE)
 
-            fname_mass = path + galactic_region+sky_region+'mass.txt'
-            fname_err = path + galactic_region+sky_region+'err.txt'
+    # return p1.communicate()
 
-            np.savetxt(fname_mass, mass)
-            np.savetxt(fname_err, mass_err)
+    p2 = subprocess.Popen([idl_command], stdin=p1.stdout, cwd=idl_code_path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-            print len(mass), sky_region, galactic_region
+    # p2 = subprocess.Popen([idl_command], stdin='\"read_files_noise_experiment(\'{0}\', {1})\"'.format(name, notrunc), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # p1.communicate()
+    output = p2.communicate()[0]
+
+    # os.chdir(cwd)
+
+
+    # output = os.system("echo \"read_files_noise_experiment(\'{0}\', {1})\" | {2}".format(name, notrunc, idl_command))
+
+    return output
+
+
+def send_mspec_command_to_idl_backup(name, notrunc=1):
+
+    idl_command = "/Applications/exelis/idl83/bin/idl"
+
+    cwd = os.getcwd()
+
+    # p1 = subprocess.Popen(["echo", '\"read_files_noise_experiment(\'{0}\', {1})\"'.format(name, notrunc)], stdout=subprocess.PIPE)
+
+    # # return p1.communicate()
+
+
+    # p2 = subprocess.Popen([idl_command], stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=idl_code_path)
+
+    # p2 = subprocess.Popen([idl_command], stdin='\"read_files_noise_experiment(\'{0}\', {1})\"'.format(name, notrunc), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # p1.communicate()
+    # output = p2.communicate()
+
+    os.chdir(idl_code_path)
+
+    output = os.system("echo \"read_files_noise_experiment(\'{0}\', {1})\" | {2}".format(name, notrunc, idl_command))
+
+    os.chdir(cwd)
+
+    return output
+
+
+def send_mspec_command_to_idl_backup2(name, notrunc=1):
+
+    idl_command = "/Applications/exelis/idl83/bin/idl"
+
+    p1 = subprocess.check_output("echo \"read_files_noise_experiment(\'{0}\', {1})\" | {2}".format(name, notrunc, idl_command))
+
+    return p1
+
+    # p1 = subprocess.Popen(["echo", '\"read_files_noise_experiment(\'{0}\', {1})\"'.format(name, notrunc)], stdout=subprocess.PIPE)
+
+    # # return p1.communicate()
+
+
+    # p2 = subprocess.Popen([idl_command], stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=idl_code_path)
+
+    # p2 = subprocess.Popen([idl_command], stdin='\"read_files_noise_experiment(\'{0}\', {1})\"'.format(name, notrunc), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # p1.communicate()
+    # output = p2.communicate()
+
+    os.chdir(idl_code_path)
+
+    output = os.system("echo \"read_files_noise_experiment(\'{0}\', {1})\" | {2}".format(name, notrunc, idl_command))
+
+    os.chdir(cwd)
+
+    return output    
+
+
+def parse_idl_output():
+    pass
