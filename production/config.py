c8bc90e9ea3acd146e8ba0431ef5f51ab9dbc8ff
Tom Rice <t.rice90@gmail.com>
2015-12-01 17:55:47 -0500

    add the paper path to config

--- production/config.py
+++ production/config.py
@@ -5,3 +5,4 @@ import os.path
 data_path = os.path.expanduser("~/Dropbox/College/Astro99/DATA/")
 idl_code_path = os.path.expanduser("~/Documents/Code/idl-low-sky/eroslib/")
 idl_executable = "/Applications/exelis/idl83/bin/idl"
+paper_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/paper/")
