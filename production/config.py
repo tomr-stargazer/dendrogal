caa8003413f11d1b9bcdd62687d511c8f1ab61a4
Tom Rice <t.rice90@gmail.com>
2015-12-08 09:16:54 -0500

    add a new path to config for data output products

--- production/config.py
+++ production/config.py
@@ -6,3 +6,4 @@ data_path = os.path.expanduser("~/Dropbox/College/Astro99/DATA/")
 idl_code_path = os.path.expanduser("~/Documents/Code/idl-low-sky/eroslib/")
 idl_executable = "/Applications/exelis/idl83/bin/idl"
 paper_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/paper/")
+catalog_output_path = os.path.expanduser("~/Dropbox/Grad School/Research/Milkyway/data_products/")
